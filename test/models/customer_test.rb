fe6112714bd06e164c017775538a72a52c01fb54
Brenna <bcmdarroch@gmail.com>
2017-05-09 15:05:55 -0700

    wrote some customer tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,47 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  let(:customer) { customers(:alison, :brenna).sample }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  it "must have a name" do
+    customer.name.must_equal customer.name
   end
+
+  it "is invalid without a name" do
+
+  end
+
+  it "must have a phone number" do
+    customer.phone.must_equal customer.phone
+  end
+
+  it "is invalid without a phone number" do
+
+  end
+
+  it "must have a registered_at date" do
+    customer.registered_at.must_equal customer.registered_at
+  end
+
+  it "is invalid without a register date" do
+
+  end
+
+  it "can have address, city, state, postal_code, account_credit" do
+    customer.address.must_equal customer.address
+    customer.city.must_equal customer.city
+    customer.state.must_equal customer.state
+    customer.postal_code.must_equal customer.postal_code
+    customer.account_credit.must_equal customer.account_credit
+  end
+
+  it "returns an array of movie rentals" do
+
+  end
+
+  it "can have multiple movie rentals" do
+
+  end
+
+
 end
