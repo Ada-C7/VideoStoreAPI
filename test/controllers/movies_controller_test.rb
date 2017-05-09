0be6be1f5aa61653ac1cc9acb9ca9d9b1da56003
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-05-09 14:55:06 -0700

    Wrote movies controller tests

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,14 +1,49 @@
 require "test_helper"
 
 describe MoviesController do
-  it "should get index" do
-    get movies_index_url
-    value(response).must_be :success?
+
+  describe "Movies#Index" do
+
+    FIELDS = %w(title release_date)
+
+    before do
+      get movies_path
+    end
+
+    it "should get index" do
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an Array" do
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the movies" do
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+
+    it "returns the correct fields" do
+      body = JSON.parse(response.body)
+      body.each do | movie |
+        movie.keys.sort.must_equal FIELDS
+      end
+    end
+
   end
 
-  it "should get show" do
-    get movies_show_url
-    value(response).must_be :success?
+  describe "Movies#Show" do
+
+    it "should get show" do
+      get movies_show_url
+      value(response).must_be :success?
+    end
+
   end
 
 end
