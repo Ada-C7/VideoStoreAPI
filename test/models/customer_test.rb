3f7ea24bc1ca29197f5eb09fbcc27922ba20722f
Ann Dai <gofarann@gmail.com>
2017-05-09 14:26:46 -0700

    customer model test

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -11,12 +11,23 @@ describe Customer do
   describe "validations" do
 
     it "must be able to create valid customer" do
-      good_data.must_be :valid?
+      good_data.save.must_equal true
     end
 
+    it "must be able to create valid customer and return corresponding data" do
+      good_data.save
+      good_data.name.must_equal "Ann D"
+    end
+
+    # it 'invalid without all parameters' do
+    #   bad_data.must_equal false
+    #   bad_data.errors.messages.must_include :city
+    # end
+
     it 'invalid without all parameters' do
-      bad_data.must_equal false
-      bad_data.errors.messages.must_include :city
+      customer_missing_name = good_data.save
+      customer_missing_name.name = nil
+      customer_missing_name.save.must_equal false
     end
 
     it 'customer requires a unique username and address (w/ city, state, zip) combo' do
@@ -31,7 +42,7 @@ describe Customer do
 
     it 'cannot create customer with negative account credit' do
       negative_account.save.must_equal false
-      good_data_dupe.errors.messages.must_include :account_credit
+      negative_account.errors.messages.must_include :account_credit
     end
 
   end
