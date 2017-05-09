3ccc5bc55814e421f7bcd885257d9e17c7a9528e
add2point71dots <alcland@gmail.com>
2017-05-09 14:20:04 -0700

    customer validations and tests

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,10 @@
 class Customer < ApplicationRecord
+  validates :name, presence: true
+  validates :registered_at, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :postal_code, presence: true
+  validates :phone, presence: true
+  validates :account_credit, presence: true
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,32 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  REQUIRED_FIELDS = %w(name registered_at address city state postal_code phone account_credit)
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  let(:new_customer) { Customer.new(
+    name: "Orange",
+    registered_at: "2015-05-09",
+    address: "123 Cool Street",
+    city: "Portland",
+    state: "OR",
+    postal_code: "22222",
+    phone: "555-555-5556",
+    account_credit: 1.23
+  ) }
+
+  describe "Validations" do
+    it "can create a customer" do
+      customer = new_customer
+      customer.must_be :valid?
+      customer.save.must_equal true
+    end
+
+    REQUIRED_FIELDS.each do |field|
+      it "#{field} is required" do
+        new_customer[field] = nil
+        new_customer.valid?.must_equal false
+        new_customer.errors.messages.must_include field.to_sym
+      end
+    end
   end
 end
