c79bf6c8c2f7b16f55fe403aa3960fbb0277f550
Regan Huff <regan.huff@gmail.com>
2017-05-09 16:30:58 -0700

    Uncomment first routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/zomg", to: "movies#index"
 
-  # get "/customers", to: "customers#index"
-  #
-  # get "/movies", to: "movies#index"
+  get "/customers", to: "customers#index"
+
+  get "/movies", to: "movies#index"
   #
   # get "/movies/:title", to: "movies#show"
   #
