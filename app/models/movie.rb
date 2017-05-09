05aa5bda194b28d7d92271d9e4772cee63428e34
add2point71dots <alcland@gmail.com>
2017-05-09 14:53:39 -0700

    movie model validations and testing

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :release_date, presence: true
+  validates :inventory, presence: true
+  validates_numericality_of :inventory, only_integer: true, greater_than_or_equal_to: 0
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,43 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  let(:new_movie) { Movie.new(
+    title: "Bubble Boy",
+    overview: "Super cool movie about a boy in a bubble.",
+    release_date: "2001-08-24",
+    inventory: 10
+  ) }
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  REQUIRED_FIELDS = %w(title release_date inventory)
+
+  describe "validations" do
+    it "can create a movie" do
+      new_movie.must_be :valid?
+      new_movie.save.must_equal true
+    end
+
+    REQUIRED_FIELDS.each do |field|
+      it "#{field} is required" do
+        new_movie[field] = nil
+        new_movie.valid?.must_equal false
+        new_movie.errors.messages.must_include field.to_sym
+      end
+    end
+
+    it "inventory must be a non-negative integer" do
+      new_movie.inventory = "number"
+      new_movie.valid?.must_equal false
+
+      new_movie.inventory = -1
+      new_movie.valid?.must_equal false
+
+      new_movie.inventory = 4.5
+      new_movie.valid?.must_equal false
+    end
+
+    it "title must be unique" do
+      new_movie.title = movies(:one).title
+      new_movie.valid?.must_equal false
+    end
   end
 end
