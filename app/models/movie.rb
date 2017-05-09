73f1289191e1f910a2e042878a0cf45774823137
Regan Huff <regan.huff@gmail.com>
2017-05-09 14:34:19 -0700

    Add tests and validations for uniqueness

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
   validates :title, presence: true
+  validates_uniqueness_of :title, scope: :release_date
   validates :inventory, presence: true
+  validates :release_date, presence: true
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -4,43 +4,46 @@ describe Movie do
   let(:movie) { Movie.new }
   let(:psycho) {movies(:psycho)}
 
-
-  # bad_data1:
-  #   overview: "Nothing happens."
-  #   release_date: 1960-06-16
-  #   inventory: 4
-  #
-  # bad_data2:
-  #   overview: "Even more of nothing happens."
-  #   release_date: 1970-04-22
-  #
-  # bad_data3:
-  #   title: "Whatevs."
   #at least one positive and one neg case
   # for each relation
   # for each validation
   describe "validations" do
-    it "must have both a title and an inventory count" do
+    it "must have a title, release date, and inventory count" do
       movie.title = "North by Northwest"
       movie.inventory = 8
+      movie.release_date = "1959-07-17"
       movie.save.must_equal true
     end
 
-    it "must not allow a movie without a title and inventory" do
+    it "must not allow a movie without a title, release date and inventory" do
       movie.overview = "Nothing happens."
-      movie.release_date = 1960-06-16
       movie.save.must_equal false
       movie.errors.messages.must_include :title
       movie.errors.messages.must_include :inventory
-
+      movie.errors.messages.must_include :release_date
     end
 
-    it "must have a unique combination of title and release date" do skip
-
+    it "must not allow just a title but no inventory or release date" do
+      movie.title = "Whatevs"
+      movie.save.must_equal false
+      movie.errors.messages.must_include :inventory
+      movie.errors.messages.must_include :release_date
     end
 
-    it "must not allow a movie with a duplicate title/release date pair" do skip
+    it "must permit a unique combination of title and release date" do
+      movie.title = "Psycho"
+      movie.overview = "The animated children's version of the Hitchcock classic"
+      movie.release_date = "2015-09-01"
+      movie.inventory = 3
+      movie.save.must_equal true
+    end
 
+    it "must not allow a movie with a duplicate title/release date pair" do
+      movie.title = psycho.title
+      movie.release_date = psycho.release_date
+      movie.inventory = 1
+      movie.save.must_equal false
+      movie.errors.messages.must_include :title
     end
 
     it "must have an inventory greater than or equal to zero" do skip
