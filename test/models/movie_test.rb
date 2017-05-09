1be146617d35a83b56f0a1dd7c23317eeaf9df7d
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-05-09 14:23:41 -0700

    Wrote movie model tests

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,25 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  let (:movie) { Movie.new(
+    title: "Finding Nemo",
+    overview: "Just keep swimming",
+    release_date: "2010",
+    inventory: "25"
+    )
+  }
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  it "Can create a valid movie" do
+    movie.must_be :valid?
+  end
+
+  REQUIRED_FIELDS = %w(title overview release_date inventory)
+
+  REQUIRED_FIELDS.each do | field |
+    it "Movie must have a #{field}" do
+      movie[field] = nil
+      movie.valid?.must_equal false
+      movie.errors.messages.must_include field.to_sym
+    end
   end
 end
