12a30e28e7d15f356d33e9881efc919a033b89a5
Chantal Rollison <rollison.chantal@gmail.com>
2017-05-09 16:56:10 -0700

    added some test outlines for the rentals controller

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "Checkout" do
+    let(:rental) {
+      {
+        movie: movies(:one).title,
+        customer: customers(:one)
+      }
+    }
+
+  it "creates a new rental" do
+    proc {
+      post rental_path(movies(:one).title)
+    }.must_change 'Rental.count', 1
+
+    must_respond_with :success
+  end
+
+  it "changes status code for rental" do
+    skip
+  end
+
+  it "changes available inventory" do
+    skip
+  end
+
+  it "sets a due date of 30 days after" do
+    skip
+  end
+
+  it "gives error message if movie isn't available" do
+    skip
+  end
+end
+
+
 end
