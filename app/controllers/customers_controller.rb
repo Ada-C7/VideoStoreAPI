48fc7ef8b06752ad5a03ec09d4214e025e12a146
Kayla <kubicke.kayla@gmail.com>
2017-05-09 16:17:23 -0700

    A list of customers can be returned.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,12 @@
 class CustomersController < ApplicationController
+
+  def index
+    customers = Customer.all
+
+    unless customers.count == 0
+      render :json => customers, status: :ok
+    else
+      render :json, status: :no_content
+    end
+  end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,47 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe 'index' do
+    it 'the route works' do
+      get customers_url
+      must_respond_with :success
+    end
+
+    it 'returns json' do
+      get customers_url
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it 'returns an array' do
+      get customers_url
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it 'returns all customers' do
+      get customers_url
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+
+    it 'returns customers with all required fields' do
+      skip
+      keys = %w(id name registered_at postal_code phone movies_checkout_out_count)
+      gets customers_url
+
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.sort.must_equal keys
+      end
+    end
+
+    it 'renders no content if customers do not exist' do
+      Customer.destroy_all
+
+      get customers_url
+      puts "#{Customer.class}"
+      assert_response :no_content
+    end
+  end
 end
