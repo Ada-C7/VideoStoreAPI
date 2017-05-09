f1e48fb57ea9ad6f631e56f33390037eee9c49cd
jjousun <jjousun@gmail.com>
2017-05-09 16:07:32 -0700

    Testing for movies controller

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "routes successfully" do
+      get movies_url
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get movies_url
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns the appropriate fields" do
+      fields = %w(title release_date)
+      get movies_url
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal fields.sort
+      end
+    end
+  end
 end
