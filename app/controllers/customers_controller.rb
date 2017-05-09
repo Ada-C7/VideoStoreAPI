ca6e245504f15c98f867179babfce5347360460f
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-05-09 16:56:40 -0700

    Added index method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,8 @@
 class CustomersController < ApplicationController
+  def index
+    movies = Customer.all
+    render json: movies.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count])
+  end
+
+
 end
