28a8ada15285e89a0773e7769360673a2b25a24a
Olivia <ssecretsharer@gmail.com>
2017-05-09 14:40:17 -0700

    added first four routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/movies", to: "movies#index", as: "movies"
+  get "/movies/:id", to: "movies#show", as: "movie"
+
+  get "/customers", to: "customers#index", as: "customers"
+  get "/customers/:id", to: "customers#show", as: "customer"
+
 end
