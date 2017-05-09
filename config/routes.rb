43317ac1ca152247a54293216572184f88c1aa27
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-05-09 15:08:25 -0700

    pseudocoded routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# GET /customers, #index
+
+# GET /movies, #index
+
+# GET /movies/:title #show
+# params: title
+
+# POST /rentals/:title/check-out
+# params: title
+# request_body: customer_id, due_date
+
+# POST /rentals/:title/check-in
+# params: title
+# request_body: customer_id
+
+# GET /rentals/overdue
+
 end
