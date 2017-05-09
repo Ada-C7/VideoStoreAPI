a8117ef274776ee42684551ac88cbfbf97cdcd7a
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-05-09 14:49:32 -0700

    Created customer controller tests, created method for customer#index

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,9 +1,39 @@
 require "test_helper"
 
 describe CustomersController do
-  it "should get index" do
-    get customers_index_url
-    value(response).must_be :success?
-  end
 
+  describe "Customers#Index" do
+
+    # need to add movies_checked_out_count
+    FIELDS = %w(name registered_at postal_code phone)
+
+    before do
+      get customers_path
+    end
+
+    it "should get index" do
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an Array" do
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the customers" do
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+
+    it "returns the correct fields" do
+      body = JSON.parse(response.body)
+      body.each do | customer |
+        customer.keys.sort.must_equal FIELDS
+      end
+    end
+  end
 end
