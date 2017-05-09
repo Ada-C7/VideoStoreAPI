4e9d8451e20f50a3dda94b04bcdd98f541aedd33
Regan Huff <regan.huff@gmail.com>
2017-05-09 14:06:33 -0700

    Stub in tests for movie model

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -2,8 +2,33 @@ require "test_helper"
 
 describe Movie do
   let(:movie) { Movie.new }
+  let(:psycho) {movies(:psycho)}
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+
+  #at least one positive and one neg case
+  # for each relation
+  # for each validation
+  describe "validations" do
+    it "must have both a title and an inventory count" do
+
+    end
+
+    it "must not allow a movie without a title and inventory" do
+
+    end
+
+    it "must have a unique combination of title and release date" do
+
+    end
+
+    it "must not allow a movie with a duplicate title/release date pair" do
+      
+    end
+  end
+
+  describe "relations" do
+    it "may have zero or more rentals" do
+
+    end
   end
 end
