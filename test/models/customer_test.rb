73909b80157377e6fa80dedbe0e18ebccb3c4177
Hyunji Kim <ricecakemonster@gmail.com>
2017-05-09 16:51:31 -0700

    customer model tests and rental model tests done

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,62 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  # let(:customer) { Customer.new }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  describe "relationships" do
+    it "can have many rentals" do
+      customer = customers(:one)
+      customer.rentals.count.must_equal 1
+    end
+
+    it "returns an empty array if there are no rentals created under this customer" do
+      customer = Customer.create(name: "who", phone: "123-1234")
+      arry = []
+      customer.rentals.must_equal arry
+    end
+
+    it "returns an empty array for movies if there are no rentals created under this customer" do
+      customer = Customer.create(name: "who", phone: "123-1234")
+      arry = []
+      customer.movies.must_equal arry
+    end
+
+    it "can have many movies" do
+      customer = customers(:one)
+      customer.movies.count.must_equal 1
+    end
+
+    it "can access movies" do
+      customer = customers(:one)
+      movie = customer.rentals[0].movie
+      customer.movies[0].title.must_equal movie.title
+    end
   end
+
+  describe "validations" do
+    it "can create a customer with a customer name and phone number" do
+       start_count = Customer.all.length
+       customer = Customer.create(name: "who", phone: "123-1234")
+       Customer.all.length.must_equal start_count + 1
+    end
+
+    it "won't create a customer without phone number or customer name" do
+       start_count = Customer.all.length
+       customer = Customer.create
+       Customer.all.length.must_equal start_count
+    end
+
+    it "won't create a customer without a customer name" do
+       start_count = Customer.all.length
+       customer = Customer.create(phone: "123-1234")
+       Customer.all.length.must_equal start_count
+    end
+
+    it "won't create a customer without a phone number" do
+       start_count = Customer.all.length
+       customer = Customer.create(name: "who")
+       Customer.all.length.must_equal start_count
+    end
+  end
+
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,9 +1,43 @@
 require "test_helper"
 
 describe Rental do
-  let(:rental) { Rental.new }
+  # let(:rental) { Rental.new }
+  describe "relationships" do
+    it "has a customer" do
+      rental = rentals(:rental_one)
+      rental.customer.name.must_equal "one"
+    end
 
-  it "must be valid" do
-    value(rental).must_be :valid?
+    it "has a movie" do
+      rental = rentals(:rental_one)
+      rental.movie.title.must_equal "Tusk"
+    end
   end
+
+  describe "validations" do
+    it "creates a rental with a movie and a customer" do
+      start_count = Rental.all.length
+      rental = Rental.create(customer_id:1, movie_id: 3)
+      Rental.all.length.must_equal start_count
+    end
+
+    it "doesn't create a rental without a movie" do
+      start_count = Rental.all.length
+      rental = Rental.create
+      Rental.all.length.must_equal start_count
+    end
+
+    it "doesn't create a rental without a customer" do
+      start_count = Rental.all.length
+      rental = Rental.create
+      Rental.all.length.must_equal start_count
+    end
+
+    it "doesn't create a rental without a movie and a customer" do
+      start_count = Rental.all.length
+      rental = Rental.create
+      Rental.all.length.must_equal start_count
+    end
+  end
+
 end
