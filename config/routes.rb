4026896b6b32279134868970fbf28c974d8e5ea3
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-05-09 14:28:14 -0700

    pseudocoded routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  # GET '/customers', to: 'customers#index', as 'customers' ## no further params needed
+  # GET '/movies', to: 'movies#index', as 'movies' ## no further params needed
+  # GET '/movies/:title', to: 'movies#show', as 'movie' ## must provide movie title
+  
 end
