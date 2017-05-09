1786d2bde2433acea54866b13a2af77dd840c6f0
Janice Lichtman <jalichtman@gmail.com>
2017-05-09 16:38:30 -0700

    created rental yml entries

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,15 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  describe "relations" do
+    it "has a list of rentals" do
+      psycho = movies(:psycho)
+      psycho.must_respond_to :rentals
+      psycho.rentals.each do |rental|
+        rental.must_be_kind_of Rental
+      end
+    end
   end
+  
 end
