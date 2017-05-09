f28cc231a9bfd38b6d400de348736ba945061ce5
Regan Huff <regan.huff@gmail.com>
2017-05-09 13:48:15 -0700

    Add sketched-out routes to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/zomg", to: "movies#index"
+
+  # get "/customers", to: "customers#index"
+  #
+  # get "/movies", to: "movies#index"
+  #
+  # get "/movies/:title", to: "movies#show"
+  #
+  # post "/rentals/:title/check-out", to: "rentals#create"
+  # [post customer_id and due_date]
+  #
+  # post "/rentals/:title/check-in", to: "rentals#update"
+  # [post customer_id]
+  #
+  # get "/rentals/overdue", to: "rentals#overdue"
 end
