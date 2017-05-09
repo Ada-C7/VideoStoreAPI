8c3bfc28355bb3054cbd57725303fb8610534da8
Regan Huff <regan.huff@gmail.com>
2017-05-09 15:40:37 -0700

    Add test to require associated movie for rental

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -2,13 +2,19 @@ require "test_helper"
 
 describe Rental do
   let(:rental) { Rental.new }
+  let(:sample_customer) {customers(:good_data)}
+  let(:sample_movie) {movies(:psycho)}
 
   it "can be created with an associated movie and customer" do skip
 
   end
 
   it "cannot be created without a movie" do
-
+    rental.customer_id = sample_customer.id
+    rental.checkout_date = Date.current
+    rental.due_date = Date.current
+    rental.save.must_equal false
+    rental.errors.messages.must_include :movie
   end
 
   it "also cannot be created without a customer" do
@@ -24,6 +30,6 @@ describe Rental do
   end
 
   it "cannot be created without a due date or checkout date" do
-    
+
   end
 end
