d0dd138f0793cb569e4e764ff68d81a510f52ee7
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-05-09 15:22:13 -0700

    Updated movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,7 @@ class MoviesController < ApplicationController
     movie = Movie.find_by(title: params[:title])
 
     if movie
-      render json: movie.as_json(only: ["title", "overview", "release_date", "inventory"]), status: :ok
+      render json: movie.as_json(only: ["inventory", "overview", "release_date", "title"]), status: :ok
     else
       render json: { nothing: true }, status: :not_found
     end
