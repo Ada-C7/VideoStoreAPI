399937ffd7b82ee5ea6f0527197fb1c33daf4e51
Marisol Lopez <lmarisolopez@gmail.com>
2017-05-09 15:09:21 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/customers", to: "customers#index", as: "customers"
+  get "/movies", to: "movies#index", as: "movies"
+  get "/movies/:title", to: "movies#index", as: "movies_by_title"
+
 end
