e9fd68422c181183fecc79cff1388f645791a87c
Ann Dai <gofarann@gmail.com>
2017-05-09 15:24:57 -0700

    all model test validations passed thus far

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,17 @@
 class Customer < ApplicationRecord
+
+  # has_many :movies
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :postal_code, presence: true
+  validates :phone, presence: true
+  validates :account_credit, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+
+
+  validates :name, uniqueness: { scope: [:address, :city, :state, :postal_code, :phone] }
+
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -3,7 +3,8 @@ require "test_helper"
 
 describe Customer do
   let(:good_data) {customers(:good_data) }
-  let(:good_data_dupe) {customers(:good_data) }
+  let(:good_data_dupe) {customers(:good_data_dupe) }
+  let(:good_data_unique) {customers(:good_data_unique) }
   let(:bad_data) {customers(:bad_data) }
   let(:negative_account) {customers(:negative_account) }
 
@@ -11,39 +12,36 @@ describe Customer do
   describe "validations" do
 
     it "must be able to create valid customer" do
-      good_data.save.must_equal true
+      # Customer.new(new_customer).save.must_equal true
+      good_data_unique.save!.must_equal true
     end
 
     it "must be able to create valid customer and return corresponding data" do
       good_data.save
-      good_data.name.must_equal "Ann D"
+      good_data.name.must_equal "John"
+    end
+
+    it 'invalid without all parameters' do
+      bad_data.save.must_equal false
+      bad_data.errors.messages.must_include :city
     end
 
     # it 'invalid without all parameters' do
-    #   bad_data.must_equal false
-    #   bad_data.errors.messages.must_include :city
+    #   customer_missing_name = good_data
+    #   customer_missing_name[:name].must_be nil
+    #   # customer_missing_name.save.must_equal false
     # end
 
-    it 'invalid without all parameters' do
-      customer_missing_name = good_data.save
-      customer_missing_name.name = nil
-      customer_missing_name.save.must_equal false
-    end
-
     it 'customer requires a unique username and address (w/ city, state, zip) combo' do
-      good_data.save!
+      good_data.save
       good_data_dupe.save.must_equal false
       good_data_dupe.errors.messages.must_include :name
-      good_data_dupe.errors.messages.must_include :address
-      good_data_dupe.errors.messages.must_include :city
-      good_data_dupe.errors.messages.must_include :state
-      good_data_dupe.errors.messages.must_include :postal_code
+
     end
 
     it 'cannot create customer with negative account credit' do
       negative_account.save.must_equal false
       negative_account.errors.messages.must_include :account_credit
     end
-
   end
 end
