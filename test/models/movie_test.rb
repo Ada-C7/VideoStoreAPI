a4a0e9555e2c7427ce6db5bafd055be1ec2939f2
sai samant <saisamant@gmail.com>
2017-05-09 16:28:31 -0700

    tests for Movie model

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,8 +1,77 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
 
-  
+    it "requires a title" do
+      titanic = movies(:one)
+      titanic.valid?.must_equal true
+      titanic.errors.messages.wont_include :title
+
+      movie2 = Movie.new( overview: "godd stuff", release_date: "2012-12-12", inventory: 2)
+      movie2.valid?.must_equal false
+      movie2.errors.messages.must_include :title
+
+    end
+
+    it "requires an overview" do
+      titanic = movies(:one)
+      titanic.valid?.must_equal true
+      titanic.errors.messages.wont_include :overview
+
+      movie2 = Movie.new( title: "godd stuff", release_date: "2012-12-12", inventory: 2)
+      movie2.valid?.must_equal false
+      movie2.errors.messages.must_include :overview
+
+
+    end
+
+    it "requires a release date" do
+      titanic = movies(:one)
+      titanic.valid?.must_equal true
+      titanic.errors.messages.wont_include :release_date
+
+      movie2 = Movie.new( title: "godd stuff", overview: "funny", inventory: 2)
+      movie2.valid?.must_equal false
+      movie2.errors.messages.must_include :release_date
+
+    end
+
+    it "requires YYYY-MM-DD format for release_date" do
+      titanic = movies(:one)
+      titanic.valid?.must_equal true
+      titanic.errors.messages.wont_include :release_date
+
+      movie2 = Movie.new( title: "good stuff", overview: "funny", release_date: "2017", inventory: 2)
+      movie2.valid?.must_equal false
+      movie2.errors.messages.must_include :release_date
+    end
+
+    it "requires an inventory" do
+      guardians = movies(:two)
+      guardians.valid?.must_equal true
+      guardians.errors.messages.wont_include :inventory
+
+      movie2 = Movie.new( title: "good stuff", overview: "funny", release_date: "2017")
+      movie2.valid?.must_equal false
+      movie2.errors.messages.must_include :inventory
+    end
+
+    it "inventory must be an integer greater than 0" do
+      guardians = movies(:two)
+      guardians.valid?.must_equal true
+      guardians.errors.messages.wont_include :inventory
+
+      movie2 = Movie.new( title: "good stuff", overview: "funny", release_date: "2017", inventory: 0)
+      movie2.valid?.must_equal false
+      movie2.errors.messages.must_include :inventory
+
+      movie2 = Movie.new( title: "good stuff", overview: "funny", release_date: "2017", inventory: 1.5)
+      movie2.valid?.must_equal false
+      movie2.errors.messages.must_include :inventory
+
+      movie2 = Movie.new( title: "good stuff", overview: "funny", release_date: "2017", inventory: -2)
+      movie2.valid?.must_equal false
+      movie2.errors.messages.must_include :inventory
+    end
 
 end
