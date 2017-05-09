32f7b229f6410a4417e675402c0a28626c1f8697
Brenna <bcmdarroch@gmail.com>
2017-05-09 13:33:04 -0700

    pseudocoded routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # route
+  # get '/customers' customers#index
+  # get '/movies' movies#index
+  # get '/movies/:title' movies#show
+
+  # post /rentals/:title/checkout
+  # post /rentals/:title/checkin
+  # get /rentals/overdue
 end
