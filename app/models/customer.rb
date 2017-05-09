bf75287fed05621872a497fe9d2c35f0e495a3a5
Emma Claire Philippides <acme9898@gmail.com>
2017-05-09 15:55:15 -0700

    zomg, validations, tests

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true
+  validates :registered_at, presence: true
 end
