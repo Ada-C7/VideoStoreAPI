f1d5e8c17e2e586782cc85de5f156cf4530066a9
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-05-09 13:13:13 -0700

    Generated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+
+  get 'movies/show'
+
+  get 'customers/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/customers", to: "customers#index"
   get "/movies", to: "movies#index"
