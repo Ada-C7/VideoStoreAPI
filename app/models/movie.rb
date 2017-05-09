538b0a6af369899213364e3b0999dd89f168143b
jjousun <jjousun@gmail.com>
2017-05-09 14:05:12 -0700

    Starting TDD for movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :overview, presence: true
+  validates :release_date, presence: true
+  validates :inventory, presence: true
+
+  
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,19 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  describe "validations" do
+    it "won't create a movie without parameters" do
+      required_attributes = [ :title, :overview, :release_date, :inventory ]
+      movie = Movie.new
+      movie.valid?.must_equal false
+      required_attributes.each do |attribute|
+        movie.errors.messages.must_include attribute
+      end
+    end
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+    it "can create a movie with valid parameters" do
+      movies(:star_wars).valid?.must_equal true
+
+    end
   end
 end
