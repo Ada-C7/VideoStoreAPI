be17996a4381d2a874b8916bdc7a609524018337
Tamiko Terada <tamikodevo@gmail.com>
2017-05-09 16:17:43 -0700

    TDD: movie tests outline as it statements

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -6,4 +6,33 @@ describe Movie do
   it "must be valid" do
     value(movie).must_be :valid?
   end
+
+  describe 'Movies#all' do
+    it "must return a collection of Movies" do
+    end
+
+    it "cannot be created without a title" do
+    end
+  end
+
+  describe 'Movies#create' do
+    it "must be created with a title" do # validaiton for :title
+    end
+
+    it "cannot be created without a title" do
+    end
+
+    it "adds a movie to our active record" do
+    end
+  end
+
+  describe 'Movies#find' do
+    it "can find a Movie with a valid title" do
+    end
+
+    it "cannot find a Movie with a bogus title" do
+    end
+  end
 end
+
+# set up fixtures
