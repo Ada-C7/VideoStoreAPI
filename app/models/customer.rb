f8e7122640d87565b553da8059d81fe6ab872f2e
Theresa Manney <theresa.m.17@gmail.com>
2017-05-09 16:17:41 -0700

    commented out movie and customer relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  has_and_belongs_to_many :movies
+  # has_and_belongs_to_many :movies
 
   validates :name, presence: true
   validates :phone, presence: true
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-  has_and_belongs_to_many :customers
+  # has_and_belongs_to_many :customers
 
   validates :title, presence: true
   validates :inventory, numericality: { only_integer: true, greater_than: -1 }
