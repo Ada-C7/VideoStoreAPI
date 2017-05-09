8298880e8fb8b60324563c929339a1271ea83ba6
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-05-09 15:41:24 -0700

    generated rental, movie, customer models and migrations

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#create a route that responds to /zomg that serves a json-encoded "it works!"
+
+#routes for customers
+
+#route for get /customers returns list of all customers with fields of info
+
+
+#routes for movies
+
+#route for get/movies - returns list of movies with fields
+
+#route for getting a single movie - parameter is a movie title, returns single moveiwith fields of info
+
+#routes for rentals?
+
+end
