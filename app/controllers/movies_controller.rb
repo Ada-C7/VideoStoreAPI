1bc9fbeb6e57128f526894d0e66d5d3c29c710d2
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-05-09 15:58:07 -0700

    Added index method to movies_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,13 @@
 class MoviesController < ApplicationController
   def zomg
     render json: "it works!"
+  end
+
+  def index
+    movies = Movie.all
+    render json: movies.as_json(except: [:created_at, :updated_at])
 
   end
+
 
 end
