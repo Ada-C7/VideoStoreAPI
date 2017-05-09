d017f12073b66322defd588ea4fbca61d4d32440
Lynn Trickey <lynn.trickey@gmail.com>
2017-05-09 16:23:27 -0700

    Dropped join table.  Did not break things.  Hopefully

---
+++ db/migrate/20170509232156_remove_join_table_customers_movies.rb
@@ -0,0 +1,5 @@
+class RemoveJoinTableCustomersMovies < ActiveRecord::Migration[5.0]
+  def change
+    drop_join_table :customers, :movies
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509230446) do
+ActiveRecord::Schema.define(version: 20170509232156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,15 +25,6 @@ ActiveRecord::Schema.define(version: 20170509230446) do
     t.string   "phone"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-  end
-
-  create_table "customers_movies", id: false, force: :cascade do |t|
-    t.integer  "customer_id", null: false
-    t.integer  "movie_id",    null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["customer_id"], name: "index_customers_movies_on_customer_id", using: :btree
-    t.index ["movie_id"], name: "index_customers_movies_on_movie_id", using: :btree
   end
 
   create_table "movies", force: :cascade do |t|
