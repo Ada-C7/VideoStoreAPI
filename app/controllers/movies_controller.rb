49486bb9a69124e72fd1d7a951168df90e8cdb65
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-05-09 16:49:24 -0700

    Added show method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,9 +5,16 @@ class MoviesController < ApplicationController
 
   def index
     movies = Movie.all
-    render json: movies.as_json(except: [:created_at, :updated_at])
-
+    render json: movies.as_json(only: [:title, :release_date])
   end
 
+  def show
+    movie = Movie.find_by(title: params[:title])
 
+    if movie
+      render json:movie.as_json(except: [:created_at,:updated_at]),status: :ok
+    else
+      render json: {nothing: true}, status: :not_found
+    end
+  end
 end
