3873aef08be3e72ffa0489b4ced7e2e5d03469eb
Tamiko Terada <tamikodevo@gmail.com>
2017-05-09 15:45:57 -0700

    notes for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   get "/zomg", to: 'customers#index'
+
+  # CUSTOMERS:
+  # /customers #all  for returning all customers
+
+  # MOVIES:
+  # /movies #all  for returning all ovies
+  # /movie #find(title)  for a movie by title
+
+  # RENTALS:
+  # (optional)
 end
