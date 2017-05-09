1c32ed823a2ce0310d5149065ed78c077e4b8eeb
Theresa Manney <theresa.m.17@gmail.com>
2017-05-09 15:53:43 -0700

    Completed movie model validation tests

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ApplicationRecord
-  belongs_to :customer
+  has_and_belongs_to_many :customers
+
+  validates :title, presence: true
+  validates :inventory, numericality: { only_integer: true, greater_than: -1 }
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -3,7 +3,16 @@ require "test_helper"
 describe Customer do
   let(:customer) { Customer.new }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  describe "Customer validations" do
+    it "Customer must have a name and phone number" do
+      customer = customers(:one)
+      customer.valid?.must_equal true
+    end
+
+    it "Customer is not valid without a name" do 
+    end
+
+    it "Customer is not valid without a phone number" do
+    end
   end
 end
