fb97a18ec1dbedfe2b1a0d41f70b17cb0b0c1471
sai samant <saisamant@gmail.com>
2017-05-09 15:57:48 -0700

    trying to fix regex in customer model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
   validates :title, presence: true
   validates :overview, presence: true
-  validates :release_date, presence: true, format: { with: /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/, message: "must be in YYYY-MM-DD format" }
+  validates :release_date, presence: true, format: { with: /\A\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])\z/, message: "must be in YYYY-MM-DD format" }
   validates :inventory, presence: true, numericality: { only_integer: true, greater_than: 0}
-  
+
 end
