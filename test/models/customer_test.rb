a5ae7f93f1c768af91dbb5fbb49a94cfb2b35cc4
Spatterjaaay <aja.pursova@gmail.com>
2017-05-09 14:00:31 -0700

    created test structure for models

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -3,7 +3,39 @@ require "test_helper"
 describe Customer do
   let(:customer) { Customer.new }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  it "you can create a customer" do
+
+  end
+
+  it "won't create customer without name" do
+
+  end
+
+  it "won't create customer without registered_at" do
+
+  end
+
+  it "won't create customer without postal_code" do
+
+  end
+
+  it "won't create customer without phone" do
+
+  end
+
+  it "won't create customer without movies_checked_out_count" do
+
+  end
+
+  it "creates customer without address" do
+
+  end
+
+  it "creates customer without city" do
+
+  end
+
+  it "creates customer without state" do
+
   end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -3,7 +3,27 @@ require "test_helper"
 describe Movie do
   let(:movie) { Movie.new }
 
-  it "must be valid" do
+  it "creates a movie" do
     value(movie).must_be :valid?
+  end
+
+  it "won't create movie without title" do
+
+  end
+
+  it "won't create movie without release_date" do
+
+  end
+
+  it "won't create movie without overview" do
+
+  end
+
+  it "won't create movie without inventory" do
+
+  end
+
+  it "won't create movie without available_inventory" do
+
   end
 end
