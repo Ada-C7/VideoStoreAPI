44b83daca6597576d7d31c28271705ec04955116
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-09 16:49:24 -0700

    added movie show request

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,10 +3,15 @@ class MoviesController < ApplicationController
     movies = Movie.all
 
     if movies.empty?
-      render json: { errors: "Something is wrong with the database"},
+      render json: { errors: "Something is wrong with the database" },
              status: :internal_server_error
     else
       render json: movies.as_json(only: [:title, :release_date]), status: :ok
     end
+  end
+
+  def show
+    movie = Movie.find_by( title: params[:title] )
+    render json: movie.as_json(only: [ :title, :overview, :release_date, :inventory, :available_inventory ]), status: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'movies', to: 'movies#index', as: 'movies'
-  get 'movies/:id', to: 'movies#show', as: 'movie'
+  get 'movies/:title', to: 'movies#show', as: 'movie'
 
 end
