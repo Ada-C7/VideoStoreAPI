bd4a230cc6ff563ab93752e22dfc9fe547497cb7
Jackie Watanabe <sushiigirl@gmail.com>
2017-05-09 16:28:33 -0700

    created tests for customer model and also created two customer fixtures

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :postal_code, length: { is: 5 }, if: :postal_code
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -3,7 +3,31 @@ require "test_helper"
 describe Customer do
   let(:customer) { Customer.new }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  it "Customer must have a name and phone number" do
+    customer = Customer.new
+    customer.valid?.must_equal false
+    customer.errors.messages.must_include :name
+    customer.errors.messages.must_include :phone
+    #value(customer).must_be :valid?
   end
+
+  it "Is valid if Customer has name and phone number" do
+    customer = Customer.new
+    customer.name = "Bob"
+    customer.phone = "808-123-4567"
+    customer.save
+    customer.valid?.must_equal true
+    customer.errors.messages.count.must_equal 0
+  end
+
+  it "postal_code must be five digits" do
+    customer = customers(:mary)
+    # customer.postal_code.valid?.must_equal true
+
+    customer2 = Customer.new
+    customer2.name = "Bob"
+    customer2.postal_code = "4383972"
+    customer2.valid?.must_equal false
+  end
+
 end
