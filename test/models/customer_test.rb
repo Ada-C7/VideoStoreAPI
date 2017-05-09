48e909b141a203206cbcc98d7fa675106192548d
Theresa Manney <theresa.m.17@gmail.com>
2017-05-09 15:56:04 -0700

    Completed customer model validations tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -9,10 +9,16 @@ describe Customer do
       customer.valid?.must_equal true
     end
 
-    it "Customer is not valid without a name" do 
+    it "Customer is not valid without a name" do
+      customer = customers(:two)
+      customer.name = nil
+      customer.valid?.must_equal false
     end
 
     it "Customer is not valid without a phone number" do
+      customer = customers(:three)
+      customer.phone = nil
+      customer.valid?.must_equal false
     end
   end
 end
