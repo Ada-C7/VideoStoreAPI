5e354410fc5cbb57e5461570d00fe24c04e9bd13
Lynn Trickey <lynn.trickey@gmail.com>
2017-05-09 14:02:49 -0700

    Able to run seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 JSON.parse(File.read('db/seeds/customers.json')).each do |customer|
-  Customer.create!(customer)
+   c = Customer.new
+   c.name = customer["name"]
+   c.registered_at = customer["registered_at"]
+   c.address = customer["address"]
+   c.city = customer["city"]
+   c.state = customer["state"]
+   c.postal_code = customer["postal_code"]
+   c.phone = customer["phone"]
+   c.save
 end
 
 JSON.parse(File.read('db/seeds/movies.json')).each do |movie|
