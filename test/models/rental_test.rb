e7482be5455da4e1b1902bd65b8b106544d100ba
Regan Huff <regan.huff@gmail.com>
2017-05-09 15:30:07 -0700

    Stub in rental model tests

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -3,7 +3,27 @@ require "test_helper"
 describe Rental do
   let(:rental) { Rental.new }
 
-  it "must be valid" do skip
-    value(rental).must_be :valid?
+  it "can be created with an associated movie and customer" do skip
+
+  end
+
+  it "cannot be created without a movie" do
+
+  end
+
+  it "also cannot be created without a customer" do
+
+  end
+
+  it "should be assigned today's date as default for checkout_date" do
+
+  end
+
+  it "should be assigned a default due date of two weeks from today" do
+
+  end
+
+  it "cannot be created without a due date or checkout date" do
+    
   end
 end
