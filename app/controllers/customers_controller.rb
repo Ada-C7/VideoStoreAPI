292b1e23aa311884cb1f8d407de3af4afe227de6
add2point71dots <alcland@gmail.com>
2017-05-09 15:23:53 -0700

    index method and tests for customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,6 @@
 class CustomersController < ApplicationController
+  def index
+    customer = Customer.all
+    render json: customer.as_json(only: [:id, :name, :registered_at, :postal_code, :phone])
+  end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  KEYS = %w(id name phone postal_code registered_at)
+
+  describe "index" do
+    it "is a real working route" do
+      get customers_path
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get customers_path
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an Array of all the customers" do
+      get customers_path
+      body = JSON.parse(response.body)
+
+      body.must_be_kind_of Array
+      body.length.must_equal Customer.count
+    end
+
+    it "returns customers with exactly the required fields" do
+      get customers_path
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.sort.must_equal KEYS
+      end
+    end
+  end
 end
