91ccd40ddcb14246d450dbdcc27c448ab183ef53
sai samant <saisamant@gmail.com>
2017-05-09 16:15:50 -0700

    tests for customer model

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,37 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+
+  describe 'validations for Customer' do
+    it 'requires a name' do
+      ada = customers(:one)
+      ada.valid?.must_equal true
+      ada.errors.messages.wont_include :name
+
+      grace = Customer.new(phone: "(415)-567-8743")
+      grace.valid?.must_equal false
+      grace.errors.messages.must_include :name
+    end
+
+
+    it 'requires a phone number' do
+      ada = customers(:one)
+      ada.valid?.must_equal true
+      ada.errors.messages.wont_include :phone
+
+      grace = Customer.new(name: "Grace Hopper")
+      grace.valid?.must_equal false
+      grace.errors.messages.must_include :phone
+
+    end
+
+    it 'requires a unique phone number' do
+      jane = Customer.new( name: "Jane Doe", phone: "(555)-555-5555" )
+      jane.valid?.must_equal false
+      jane.errors.messages.must_include :phone
+    end
+
+
   end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -3,7 +3,6 @@ require "test_helper"
 describe Movie do
   let(:movie) { Movie.new }
 
-  it "must be valid" do
-    value(movie).must_be :valid?
-  end
+  
+
 end
