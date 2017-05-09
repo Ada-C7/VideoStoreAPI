563e704f00e5f1dabea1437ba338f1ff2f4d31d1
Bo Trethewey <botrethewey@gmail.com>
2017-05-09 16:34:12 -0700

    initially generted controllers for customers and movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+
+  get 'movies/show'
+
+  get 'customers/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/zomg', to: 'application#index'
 end
