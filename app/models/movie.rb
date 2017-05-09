f3c1a31c730c26200e2ed93189aa9d1288750330
Lynn Trickey <lynn.trickey@gmail.com>
2017-05-09 14:09:58 -0700

    Created initial movie model tests

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ApplicationRecord
   has_and_belongs_to_many :customer
+  validates :title, presence: true, uniqueness: true
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -3,7 +3,21 @@ require "test_helper"
 describe Movie do
   let(:movie) { Movie.new }
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  it "requires a title to create" do
+    movie.valid?.must_equal false
   end
+
+  it "title must be unique" do
+    bad_movie = Movie.new
+    bad_movie.title = "American Beauty"
+
+    bad_movie.save.must_equal false
+  end
+
+  it "must be able to create w/title" do
+    movie.title = "My Movie"
+
+    movie.valid?.must_equal true
+  end
+
 end
