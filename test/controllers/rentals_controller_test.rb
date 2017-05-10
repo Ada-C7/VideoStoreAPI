a8d152b8c1240d215f940174ee051ac2d24495c0
Regan Huff <regan.huff@gmail.com>
2017-05-10 13:22:52 -0700

    Stub in tests for checkin

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -4,4 +4,26 @@ describe RentalsController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+
+  describe "check_in" do
+    it "succeeds for valid title and customer id" do
+      #valid means: rental exists [and therefore movie and customer exist]
+    end
+
+    it "modifies the db with today's date as checkin date" do
+
+    end
+
+    it "renders bad request for invalid customer id data" do
+      #customer id not found
+    end
+
+    it "renders a different message if rental does not exist" do
+      #rental does not exist (customer does not actually have that movie checked out)
+    end
+
+    it "returns status not found if movie not found" do
+      #movie title does not return movie
+    end
+  end
 end
