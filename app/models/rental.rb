bd94fe34683fb356e8f70655f96801dec94c432a
Ashtn <ashtonehorton@gmail.com>
2017-05-10 13:58:35 -0700

    added model tests for rental

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,5 @@
 class Rental < ApplicationRecord
+  belongs_to :movie
+  belongs_to :customer
+  validates :due_date, presence: true
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -2,8 +2,55 @@ require "test_helper"
 
 describe Rental do
   let(:rental) { Rental.new }
+  let(:rental2) {Rental.new(customer_id: customers(:one).id, movie_id: movies(:one).id, due_date: "2017-05-10" )}
 
-  it "must be valid" do
-    value(rental).must_be :valid?
+  RENTAL_VALIDATIONS = %w( customer_id movie_id due_date)
+
+  it ' Will create a new rental with valid inputs' do
+    rental2.save.must_equal true
+    rental2.customer_id.must_equal customers(:one).id
+    rental2.movie_id.must_equal movies(:one).id
+    rental2.due_date.must_equal DateTime.parse("2017-05-10")
   end
+
+
+  it " Won't create a rental without validated information" do
+    rental.save.must_equal false
+  end
+
+
+  it " Customers must be present " do
+    rental2.customer_id = nil
+    rental2.valid?.must_equal false
+    rental2.errors.messages.must_include :customer
+  end
+
+  it " Movie must be present " do
+    rental2.movie_id = nil
+    rental2.valid?.must_equal false
+    rental2.errors.messages.must_include :movie
+  end
+
+  it " Due date must be present " do
+    rental2.due_date = nil
+    rental2.valid?.must_equal false
+    rental2.errors.messages.must_include :due_date
+  end
+
+  describe "Rental Relationships" do
+
+    it "Has access to customer object" do
+      rental2.customer.must_be_instance_of Customer
+      rental2.customer.phone.present?.must_equal true
+      rental2.customer.phone.must_equal customers(:one).phone
+    end
+
+    it " Has access to movie object" do
+      rental2.movie.must_be_instance_of Movie
+      rental2.movie.title.present?.must_equal true
+      rental2.movie.title.must_equal movies(:one).title
+    end
+
+  end
+
 end
