e4b19a1d9a6f5e92601691d3fc0ca62fc7be6790
natalia-ku <bilberryfm@mail.ru>
2017-05-10 12:03:04 -0700

    added create method for rentals

---
+++ app/controllers/rentals_controller.rb
@@ -0,0 +1,51 @@
+require 'date'
+
+class RentalsController < ApplicationController
+
+  def create
+    rental = Rental.new
+    movie = Movie.find_by(title: params[:title])
+    rental.movie_id = movie.id
+    rental.check_out_date = Date.today
+    # rental.return_date = nil
+    rental.due_date = check_out_date + 3
+    rental.status = "checked out"
+
+    rental.customer_id = params[:customer_id]
+    customer = Customer.find(params[:customer_id])
+
+    customer.movies_checked_out_count += 1
+    # movie.available_inventory = inventory - 1 # ??????????
+    if rental.save
+      render status: :ok, json: { id: rental.id }
+    else
+      render status: :bad_request, json: { errors: rental.errors.messages }
+    end
+  end
+
+  def update
+    rental = Rental.find_by(title: params[:title], customer_id: params[:customer_id])
+    rental.return_date = Date.today
+
+    rental.customer.movies_checked_out_count += 1
+    rental.movie.available_inventory += 1 # ??????????
+    rental.status = "checked in"
+
+    if rental.save
+      render status: :ok, json: { id: rental.status }
+    else
+      render status: :bad_request, json: { errors: rental.errors.messages }
+    end
+  end
+
+
+
+
+
+
+  private
+  def rental_params
+    params.require(:rental).permit(:customer_id, :title, :check_out_date,
+    :return_date, :due_date, :status)
+  end
+end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,6 +1,8 @@
 class Rental < ApplicationRecord
+  belongs_to :movie
+  belongs_to :customer
   validates :check_out_date, presence: true
-  validates :return_date, presence: true
+  # validates :return_date, presence: true
   validates :due_date, presence: true
   validates :status , presence: true, inclusion: { in: [ "checked out", "checked in", "overdue"] }
 end
