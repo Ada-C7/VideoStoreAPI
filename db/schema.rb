8a1a9d3566994f3275da3556938d62432418407c
Ashtn <ashtonehorton@gmail.com>
2017-05-10 13:26:01 -0700

    added a column i forgot whoooops!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510194600) do
+ActiveRecord::Schema.define(version: 20170510202328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170510194600) do
     t.datetime "check_in"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.datetime "due_date"
     t.index ["customer_id"], name: "index_rentals_on_customer_id", using: :btree
     t.index ["movie_id"], name: "index_rentals_on_movie_id", using: :btree
   end
