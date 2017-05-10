cd4dabad42746a33f5a53949bb33473a392a2676
Sahana Murthy <s.murthy730.com>
2017-05-10 11:35:31 -0700

    All customer model tests passing

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -18,10 +18,69 @@ describe Customer do
       customer.valid?.must_equal false
     end
 
+    it "is valid with a name" do
+      customer = customers(:sahana1)
+      customer.valid?.must_equal true
+    end
+
     it "is invalid without a phone number" do
       customer = customers(:tamiko)
       customer.valid?.must_equal false
     end
 
+    it "is valid with a phone number" do
+      customer = customers(:tamiko1)
+      customer.valid?.must_equal true
+    end
+
+    it "is invalid without a registered_at date" do
+      customer = customers(:bahana)
+      customer.valid?.must_equal false
+    end
+
+    it "is valid with a registered_at date" do
+      customer = customers(:perfect)
+      customer.valid?.must_equal true
+    end
+
+    it "is valid without a postal code" do
+      customer = customers(:no_postal)
+      customer.valid?.must_equal true
+    end
+
+    it "is valid with a 5 character postal code" do
+      customer = customers(:perfect)
+      customer.valid?.must_equal true
+    end
+
+    it "is invalid without a 5 character postal code" do
+      customer = customers(:four)
+      customer.valid?.must_equal false
+
+      customer_one = customers(:six)
+      customer_one.valid?.must_equal false
+    end
+
+    it "is invalid without an account_credit" do
+      customer = customers(:no_credit)
+      customer.valid?.must_equal false
+    end
+
+    # Tried to test and invalidate strings, but found out strings will automatically get converted to a zero value
+    # it "is invalid with a non-numerical account_credit" do
+    #   customer = customers(:string_credit)
+    #   binding.pry
+    #   customer.valid?.must_equal false
+    # end
+
+    it "is invalid with a negative account_credit" do
+      customer = customers(:negative_credit)
+      customer.valid?.must_equal false
+    end
+
+    it "is valid with a 0 account_credit value" do
+      customer = customers(:zero_credit)
+      customer.valid?.must_equal true
+    end
   end
 end
