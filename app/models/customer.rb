2d37a9e4278e345cd9855078a3889b64b3ca13ce
Bo Trethewey <botrethewey@gmail.com>
2017-05-10 13:01:57 -0700

    wip: added custom method to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,10 @@
 class Customer < ApplicationRecord
   validates :name, presence: true
   validates :phone, presence: true, uniqueness: true
+
+  # update later after adding rental
+  def movies_checked_out_count
+    return 0
+  end
+
 end
