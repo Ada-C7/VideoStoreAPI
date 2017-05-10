4e4757f86f8efbe58c96a7970bd60cf00c5ae285
sai samant <saisamant@gmail.com>
2017-05-10 14:25:29 -0700

    started model tests for rental

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,19 @@
 class Rental < ApplicationRecord
+  belongs_to :customer
+  belongs_to :movie
+
+  validates :due_date, presence: true
+  validate :valid_due_date
+
+
+
+  def valid_due_date
+    if due_date.class == Date && due_date > Date.current
+      return true
+    else
+      return false
+    end
+  end
+
+
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,9 +1,51 @@
 require "test_helper"
 
 describe Rental do
-  let(:rental) { Rental.new }
 
-  it "must be valid" do
-    value(rental).must_be :valid?
+  describe "validations for rental" do
+    it "requires a due date" do
+      rental = rentals(:one)
+      puts rental.errors.messages
+      rental.valid?.must_equal true
+
+      rental = Rental.new(customer: customers(:one), movie: movies(:one))
+      rental.valid?.must_equal false
+      rental.errors.messages.must_include :due_date
+
+    end
+
+    it "requires a due date after today" do
+
+    end
+
+    it "due date must be a Date object" do
+
+    end
+
+    it "requires a returned field" do
+
+    end
+
+    it "returned is set to false" do
+
+    end
+
+    describe "relationships" do
+      it "requires a customer" do
+
+      end
+
+      it "requires a movie" do
+
+      end
+
+      it "can get the customer" do
+      end
+
+      it "can get the movie" do
+
+      end
+
+    end
   end
 end
