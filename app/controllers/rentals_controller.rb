aacf768b4dd48576957ffb95b549ebe65025b86a
Alix Hamilton <alix.hamilton@gmail.com>
2017-05-10 15:27:38 -0700

    adds checkout and tests to rentals

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,15 @@
 class RentalsController < ApplicationController
+  def check_out
+    movie = Movie.find_by(title: params[:title])
+    customer = Customer.find_by_id(params[:customer_id])
+    due_date = params[:due_date]
+
+    rental = Rental.new(due_date: due_date, customer: customer, movie: movie)
+
+    if rental.save
+      render status: :ok, json: rental.as_json(except: [:updated_at, :created_at])
+    else
+      render status: :bad_request, json: { errors: rental.errors.messages }
+    end
+  end
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -2,13 +2,35 @@ require "test_helper"
 
 describe RentalsController do
   describe "check_out" do
-    it "routes successfully" do
-      post check_out_url(movies(:star_wars).title)
+    let(:rental_data) {
+      {
+        customer_id: customers(:aj).id,
+        due_date: "2017-05-22"
+      }
+    }
+
+    it "creates a new rental" do
+      proc {
+        post check_out_url(movies(:star_wars).title), params: rental_data
+      }.must_change 'Rental.count', 1
       must_respond_with :success
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include "id"
+
+      Rental.find(body["id"]).due_date.must_equal rental_data[:due_date]
     end
 
-    it "text" do
+    it "does not create a new rental if data is missing" do
+      rental_data.delete(:customer_id)
+      proc {
+        post check_out_url(movies(:star_wars).title), params: rental_data
+      }.wont_change 'Rental.count'
+      must_respond_with :bad_request
 
+      body = JSON.parse(response.body)
+      body["errors"].must_include "customer"
     end
   end
 end
