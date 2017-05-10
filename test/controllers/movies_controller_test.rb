cc59455957150f9fa1aa0fc76963dda261640e59
Ann Dai <gofarann@gmail.com>
2017-05-10 10:55:20 -0700

    added test stubs for checkout method

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 describe MoviesController do
 
-  KEYS = %w(release_date title) #if we loop through this, we can see whether the keys and values work/exist
+  MOVIE_KEYS = %w(release_date title) #if we loop through this, we can see whether the keys and values work/exist
 
   describe 'index' do
     it "is a real wokring route" do
@@ -33,9 +33,35 @@ describe MoviesController do
       get movies_url
       body = JSON.parse(response.body)
       body.each do |movie|
-        movie.keys.sort.must_equal KEYS
+        movie.keys.sort.must_equal MOVIE_KEYS
       end
     end
   end
+
+  # describe 'show' do
+  #   it "can get a movie" do
+  #     get movie_path(Movie.all.last.id)
+  #     must_respond_with :success
+  #   end
+  #
+  #   it "returns JSON when movie not found" do
+  #     get movie_path(Movie.all.last.id+1)
+  #     must_respond_with :not_found
+  #
+  #     body = JSON.parse(response.body)
+  #     body.must_equal "not found" => true
+  #   end
+  #
+  #   it "returns the right movie when id" do
+  #     get movie_path(Movie.all.first.id)
+  #
+  #     body = JSON.parse(response.body)
+  #
+  #     MOVIE_KEYS.each do |key|
+  #       body[key].must_equal Movie.all.first[key]
+  #     end
+  #   end
+  #
+  # end
 
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "Create (checkout)" do
+
+    it "given valid customer id and movie id, can checkout movie" do
+    end
+
+    it "given invalide customer_id, cannot checkout movie" do
+    end
+
+    it "given invalide movie_id, cannot checkout movie" do
+    end
+
+    it "can checkout a movie with enough inventory" do
+    end
+
+    it "cannot checkout movie with no inventory" do
+    end
+
+    it "returns a due_date that is 2 weeks from the checkout date" do
+
+    end
+
+
+  end
 end
