13589da3f62e3a5955a3d356ed6e4eade184ee94
Marisol Lopez <lmarisolopez@gmail.com>
2017-05-10 09:02:11 -0700

    migration for rentals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509215753) do
+ActiveRecord::Schema.define(version: 20170509235238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,17 @@ ActiveRecord::Schema.define(version: 20170509215753) do
     t.integer  "available_inventory"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "customer_id"
+    t.string   "name"
+    t.string   "postal_code"
+    t.string   "checkout_date"
+    t.string   "due_date"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
