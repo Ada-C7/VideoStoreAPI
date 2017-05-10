9f3c356bebf0516e4d498c09e8cd9e7536832746
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-05-10 11:36:09 -0700

    movie tests

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 describe Movie do
-  
+
   hash = {
     title: "test_title",
     overview: "Long Synopsis",
@@ -15,19 +15,19 @@ describe Movie do
     value(movie).must_be :valid?
   end
 
-  it "takes a title" do
+  it "requires a title" do
     movie.title.must_equal "test_title"
   end
 
-  it "takes an overview" do
+  it "requires an overview" do
     movie.overview.must_equal "Long Synopsis"
   end
 
-  it "take a release_date" do
+  it "requires a release_date" do
     movie.release_date.must_equal "1975-06-19"
   end
 
-  it "takes an inventory" do
+  it "requires an inventory" do
     movie.inventory.must_equal 8
   end
 
