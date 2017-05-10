5df1f29f8cf03908d3ef49401e711225a08cf8aa
amb54 <ambarklund@hotmail.com>
2017-05-10 15:04:27 -0700

    Updated the tests for the customer model and the customer controller.

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,55 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
-end
+
+  describe "index" do
+    it "is a working route" do
+      get customers_url
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get customers_url
+      response.header['Content-Type'].must_include 'json'
+      response.header['Content-Type'].must_include 'application/json'
+    end
+
+    it "returns an array" do
+      get customers_url
+      # response.body.must_be_kind_of Array  # response.body is a string
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all customers" do
+      get customers_url
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+  end # END of describe "index"
+
+  describe "output from customer_serializer" do
+
+    it "returns value for name" do
+      get customers_url
+      body = JSON.parse(response.body)
+      body[0].must_include "name"
+    end
+
+    it "returns value for movies_checked_out_count" do
+      get customers_url
+      body = JSON.parse(response.body)
+      body[0].must_include "movies_checked_out_count"
+    end
+
+    it "will not return value for city" do
+      get customers_url
+      body = JSON.parse(response.body)
+      body[0].wont_include "city"
+    end
+
+  end # END of describe "output from customer_serializer" 
+
+
+
+end # END of describe CustomersController
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -6,39 +6,58 @@ describe Customer do
   # it "must be valid" do
   #   value(customer).must_be :valid?
   # end
-describe "validations" do
-  it "must have a name" do
-    customer = Customer.new(address: "234 flower street", phone: "000-768-6252", registered_at: "some time")
-    customer.valid?.must_equal false
-    customer.errors.messages.must_include :name
+  describe "validations" do
+    it "must have a name" do
+      customer = Customer.new(address: "234 flower street", phone: "(000) 768-6252", registered_at: "some time")
+      customer.valid?.must_equal false
+      customer.errors.messages.must_include :name
 
+    end
+
+    it "name must be a string, returns false when integers are entered" do
+      customer1 = Customer.new(name: 212, address: "234 flower street", phone: "(000) 768-6252", registered_at: "some time")
+      customer1.valid?.must_equal false
+      customer1.errors.messages.must_include :name
+      customer2 = Customer.new(name: "Adam the 2nd", address: "234 flower street", phone: "(000) 768-6252", registered_at: "some time")
+      customer2.valid?.must_equal false
+      customer2.errors.messages.must_include :name
+      customer3 = Customer.new(name: "Adam Smith", address: "234 flower street", phone: "(000) 768-6252", registered_at: "some time")
+      customer3.valid?.must_equal true
+    end
+
+
+
+    it "name must be a string only including letters and spaces" do
+      customer = Customer.new(name: 212, address: "234 flower street", phone: "(000) 768-6252", registered_at: "some time")
+      customer.valid?.must_equal false
+      customer.errors.messages.must_include :name
+    end
+
+    it "must have a street address" do
+      customer = Customer.new(name: "Anna", phone: "(000) 768-6252", registered_at: "some time")
+      customer.valid?.must_equal false
+      customer.errors.messages.must_include :address
+    end
+
+    it "must have a phone number" do
+      customer = Customer.new(name: "Anna", address: "234 flower street", registered_at: "some time")
+      customer.valid?.must_equal false
+      customer.errors.messages.must_include :phone
+
+    end
+
+    it "must have a registered_at " do
+      customer = Customer.new(name: "Anna", address: "234 flower street", phone: "(000) 768-6252")
+      customer.valid?.must_equal false
+      customer.errors.messages.must_include :registered_at
+    end
+  end # END of describe "validations"
+
+  describe "#movies_checked_out_count" do
+    it "first test: returns 0" do
+      customer = customers(:one)
+      customer.movies_checked_out_count.must_equal 0
+    end
   end
-
-  it "name must be a string" do
-    customer = Customer.new(name: 212, address: "234 flower street", phone: "000-768-6252", registered_at: "some time")
-    customer.valid?.must_equal false
-    customer.errors.messages.must_include :name
-
-  end
-
-  it "must have a street address" do
-    customer = Customer.new(name: "Anna", phone: "000-768-6252", registered_at: "some time")
-    customer.valid?.must_equal false
-    customer.errors.messages.must_include :address
-  end
-
-  it "must have a phone number" do
-    customer = Customer.new(name: "Anna", address: "234 flower street", registered_at: "some time")
-    customer.valid?.must_equal false
-    customer.errors.messages.must_include :phone
-
-  end
-
-it "must have a registered_at " do
-  customer = Customer.new(name: "Anna", address: "234 flower street", phone: "000-768-6252")
-  customer.valid?.must_equal false
-  customer.errors.messages.must_include :registered_at
-end
-end # END of describe "validations"
 
 end
