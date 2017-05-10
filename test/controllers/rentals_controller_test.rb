5e3812fd11369bcb00d539d0df830f0b84fc6de9
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-10 15:48:53 -0700

    added to rentals create success test

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -13,7 +13,11 @@ describe RentalsController do
     it 'creates a new rental' do
       post rental_path(@movie.title), params: { rental: rental_data }
       must_respond_with :success
+      response.parsed_body.must_include "id"
+      Rental.find(response.parsed_body["id"]).movie_id.must_equal @movie.id
+      Rental.find(response.parsed_body["id"]).customer_id.must_equal @customer.id
     end
+
 
   end
 end
