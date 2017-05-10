a0302c01d4943b953f391589297d95a2e992ee07
Andrea Valliere <amvalliere@gmail.com>
2017-05-10 10:25:42 -0700

    Generated controllers for customers and movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+
+  get 'movies/show'
+
+  get 'customers/index'
+
+  get 'customers/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
