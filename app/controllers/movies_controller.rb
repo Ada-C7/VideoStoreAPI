4a16b11ee213cb2cff581266675e67ba132a0a3a
Regan Huff <regan.huff@gmail.com>
2017-05-10 12:34:48 -0700

    Additional tests and action for movie show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,17 @@ class MoviesController < ApplicationController
     movies = Movie.all
     render json: movies.as_json(only: [:title, :release_date]), status: :ok
   end
+
+  def show
+    # this might be better if titles can be duplicate
+    # returns an array of instances rather than an instance
+    # movie = Movie.where({title: params[:title]})
+
+    movie = Movie.find_by(title: params[:title])
+
+    if movie
+      render json: movie.as_json(except: [:id, :updated_at, :created_at],
+      methods: :available_inventory), status: :ok
+    end
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -6,32 +6,29 @@ describe MoviesController do
   MOVIE_SHOW_KEYS = %w(available_inventory inventory overview release_date title)
 
   describe 'index' do
-    it "is a real wokring route" do
+    before do
       get movies_url
+    end
+
+    it "is a real working route" do
       must_respond_with :success
     end
 
     it "returns json" do
-      get movies_url
       response.header['Content-Type'].must_include 'json'
     end
 
     it "returns an array" do
-      get movies_url
-
       body = JSON.parse(response.body)
       body.must_be_kind_of Array
     end
 
-    it "returns all fo the Movies" do
-      get movies_url
-
+    it "returns all of the movies" do
       body = JSON.parse(response.body)
       body.length.must_equal Movie.count
     end
 
     it "returns movies with the required fields" do
-      get movies_url
       body = JSON.parse(response.body)
       body.each do |movie|
         movie.keys.sort.must_equal MOVIE_INDEX_KEYS
@@ -54,12 +51,20 @@ describe MoviesController do
       body.keys.sort.must_equal MOVIE_SHOW_KEYS
     end
 
-    it "should return a movie with the right information" do skip
+    it "should return a movie with the right information" do
+      body = JSON.parse(response.body)
 
+      MOVIE_SHOW_KEYS.each do |key|
+          next if key == "available_inventory"
+        body[key].must_equal arabia[key]
+      end
     end
 
-    it "should return not found if the movie is not found" do skip
-
+    it "should return status not found if the movie is not found" do skip
+      get movie_path("xqrt99")
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+      body.must_equal {}
     end
   end
 
