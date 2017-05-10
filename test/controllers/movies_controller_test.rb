4c898f5ed97c6efa07bc6b6b8919fcfc0ba0dd09
Theresa Manney <theresa.m.17@gmail.com>
2017-05-10 11:22:38 -0700

    Completed movies controller tests, need negative tests for movies index

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,67 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  ALL_MOVIES = %w(release_date title)
+  ONE_MOVIE = %w(id inventory overview release_date title)
+
+  describe "index" do
+    it "is a real working route" do
+      get movies_url
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get movies_url
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an Array" do
+      get movies_url
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the movies" do
+      get movies_url
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+
+    it "returns movies with exactly the required fields" do
+      get movies_url
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal ALL_MOVIES
+      end
+    end
+  end
+
+  describe "show" do
+    it "must get a movie" do
+      get movie_path(movies(:one).title)
+      must_respond_with :success
+
+      body = JSON.parse(response.body)
+      body.must_be_instance_of Hash
+      body.keys.sort.must_equal ONE_MOVIE
+    end
+
+    it "Responds correctly when the movie is not found" do
+      get movie_path( Movie.all.last.id + 1 )
+
+      body = JSON.parse(response.body)
+      body["nothing"].must_equal true
+
+      must_respond_with :not_found
+    end
+
+    it "When we get a movie it has the right information" do
+      get movie_path(movies(:one).title)
+      body = JSON.parse(response.body)
+
+      ONE_MOVIE.each do |key|
+        body[key].must_equal movies(:one)[key]
+      end
+    end
+  end
 end
