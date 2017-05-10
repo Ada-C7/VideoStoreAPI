b5918e8049bab928e7a50220e87af52efa9ea074
Tehut Getahun <tehut.m.g@gmail.com>
2017-05-09 19:45:41 -0700

    created routes and controller actions for movie#show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,16 @@ class MoviesController < ApplicationController
   def zomg
     render :json => {message: "it works!"}, status: :ok
   end
+
+
+
+  def show
+    @movie = Movie.find_by(title: params[:title])
+    if @movie
+    render :json=> @movie.as_json(only: [:title, :release_date,:overview, :inventory]), status: :ok
+    else
+      render :json=>@movie.as_json, status: :not_found
+    end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get "zomg", to: "movies#zomg", as: "zomg"
+get "movies/:title", to:'movies#show', as:"movie"
 
   end
