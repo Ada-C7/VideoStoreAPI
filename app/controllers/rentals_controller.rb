3537ec2e215a13378a7d173eae32d4d3efa6bdce
sai samant <saisamant@gmail.com>
2017-05-10 15:57:28 -0700

    testing rental model validations for due date

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,5 +1,13 @@
 class RentalsController < ApplicationController
   def create
+    rental = Rental.new(rental_params)
+
+    if rental.save
+      render status: :ok, json: {rental_success: true}
+    else
+      render status: :bad_request, json: { errors: rental.errors.messages }
+    end
+
   end
 
   def update
@@ -7,4 +15,11 @@ class RentalsController < ApplicationController
 
   def overdue
   end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:due_date, :customer_id, :title)
+  end
+
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -5,21 +5,25 @@ describe Rental do
   describe "validations for rental" do
     it "requires a due date" do
       rental = rentals(:one)
-      puts rental.errors.messages
       rental.valid?.must_equal true
 
       rental = Rental.new(customer: customers(:one), movie: movies(:one))
       rental.valid?.must_equal false
       rental.errors.messages.must_include :due_date
 
+      rental = Rental.new(customer: customers(:one), movie: movies(:one), due_date: "2017")
+      rental.valid?.must_equal false
+      rental.errors.messages[:due_date].must_include "date must be in YYYY-MM-DD format"
     end
 
     it "requires a due date after today" do
-
+      rental = Rental.new(customer: customers(:one), movie: movies(:one), due_date: "2016-04-13")
+      rental.valid?.must_equal false
+      rental.errors.messages[:due_date].must_equal ["due date must be after today"]
     end
 
     it "due date must be a Date object" do
-
+      
     end
 
     it "requires a returned field" do
@@ -29,6 +33,7 @@ describe Rental do
     it "returned is set to false" do
 
     end
+end
 
     describe "relationships" do
       it "requires a customer" do
@@ -47,5 +52,5 @@ describe Rental do
       end
 
     end
-  end
+
 end
