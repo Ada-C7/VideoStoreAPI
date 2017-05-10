8915dbca7ec925f98f1c90f5acc1b7c85889c98b
natalia-ku <bilberryfm@mail.ru>
2017-05-10 10:38:40 -0700

    typo in movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,5 +3,5 @@ class Movie < ApplicationRecord
   validates :overview, presence: true
   validates :release_date, presence: true
   validates :inventory, presence: true, numericality: { greater_than: -1 }
-  validates :available_invetory, presence: true, numericality: { greater_than: -1 }
+  validates :available_inventory, presence: true, numericality: { greater_than: -1 }
 end
