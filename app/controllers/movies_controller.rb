a5e0398751d2158f91f4d5da3d64391da6b51d3d
EricaJCase <ericajcase@gmail.com>
2017-05-10 10:17:31 -0700

    mid-progress commit to pull Hyunji's work

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,26 @@
 class MoviesController < ApplicationController
+  def index
+    movies = Movie.all
+    render json: movies, status: :ok
+  end
+
+  def show
+    movie = movie.find_by(id: params[:id])
+
+    if movie
+      render json: movie.as_json(only: [:id, :name, :age, :human]), status: :ok
+    else
+      render json: movie,  status: :no_content
+    end
+  end
+
+  def create
+    movie = movie.new(movie_params)
+    movie.save!
+    render status: :ok, json: { id: movie.id }
+  end
+  private
+  def movie_params
+    params.require(:movie).permit(:name, :age, :human)
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,62 @@
-require "test_helper"
+require 'test_helper'
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "is a real working route" do
+      get movies_path
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get movies_url
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an Array" do
+      get movies_url
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the movies" do
+      get movies_url
+
+      body = JSON.parse(response.body)
+      body.length.must_equal movie.count
+    end
+
+    it "returns movies with exactly the required fields" do
+      keys = %w(age human id name)
+      get movies_url
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal keys
+      end
+    end
+  end
+
+  describe "show" do
+    # This bit is up to you!
+    it "can get a movie" do
+      get movie_path(movies(:two).id)
+      must_respond_with :success
+    end
+
+    it "returns a 204 for a non-existant movie" do
+      get movie_path(120)
+      must_respond_with :no_content
+    end
+  end
+
+  describe "create" do
+    let(:movie_data) {
+      {
+        name: "Jack",
+        age: 7,
+        human: "Captain Barbossa"
+      }
+    }
+
+  end
 end
