cbb0d02c1c1fd75fadfc397d2b8e5f3989ad669c
jjousun <jjousun@gmail.com>
2017-05-10 13:42:45 -0700

    Testing valid date formats

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,5 +1,11 @@
 class Rental < ApplicationRecord
   validates :checkout_date, presence: true
+  validates_format_of :checkout_date, :with => /\d{4}\-\d{2}\-\d{2}/, :message => "Checkout date must be in the following format: yyyy/mm/dd"
   belongs_to :movie
   belongs_to :customer
+
+  def due_date
+    # checkout-date plus 3 days?
+    # "2017-05-13"
+  end
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -14,6 +14,18 @@ describe Rental do
     it "can create a rental with valid parameters" do
       rentals(:one).valid?.must_equal true
     end
+
+    it "cannot create rental with invalid checkout date format" do
+      bad_rental = Rental.new(checkout_date: "05-13-2017", customer: customers(:aj), movie: movies(:star_wars))
+      bad_rental.valid?.must_equal false
+    end
+  end
+
+  describe "due_date" do
+    it "calculates the due date on initialization" do
+      expected_due_date = "2017-05-13"
+      rentals(:one).due_date.must_equal expected_due_date
+    end
   end
 
 end
