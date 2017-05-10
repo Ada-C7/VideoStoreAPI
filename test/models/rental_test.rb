b488bb91b1741159b9dcff32cafdd66c3eb7bcc6
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-10 11:29:05 -0700

    success test for rental validations

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,9 +1,24 @@
 require "test_helper"
+require 'date'
 
 describe Rental do
-  let(:rental) { Rental.new }
 
-  it "must be valid" do
-    value(rental).must_be :valid?
+  before do
+    @checkout
+  end
+
+  let(:movie) { movies(:one) }
+  let(:customer) { customers(:good_customer) }
+
+  it "requires all attributes" do
+    rental = Rental.new
+    rental.movie_id = movie.id
+    rental.customer_id = customer.id
+    rental.check_out_date = Date.today
+    rental.due_date = Date.today + 3
+    rental.status = "checked out"
+    rental.valid?.must_equal true
+    rental.save
+    Rental.find_by(customer_id: customer.id).wont_be_nil
   end
 end
