aca08fed925437f5e5c6142f7924aa8360e1a055
Olivia <ssecretsharer@gmail.com>
2017-05-10 13:03:45 -0700

    Stubbed out controller tests

---
+++ test/controllers/movies_controller_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+describe MoviesController do
+
+  describe "What a Movie do" do
+
+    it "Movies it should get(send) an index" do
+
+    end
+
+    it "should get(send)a show for individual movie" do
+
+    end
+
+    it "should show a newly created movie object" do
+
+    end
+  end
+
+
+end
