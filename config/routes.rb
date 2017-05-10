a50af2fc18b7b45dfef6b450f41f4a0147fabd26
Olivia <ssecretsharer@gmail.com>
2017-05-10 14:09:55 -0700

    added post route for movies create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  get "/movies", to: "movies#index", as: "movies"
-  get "/movies/:id", to: "movies#show", as: "movie"
+  get "movies", to: "movies#index", as: "movies"
+  get "movies/:id", to: "movies#show", as: "movie"
+  post "/movies", to:"movies#create"
 
   get "/customers", to: "customers#index", as: "customers"
   get "/customers/:id", to: "customers#show", as: "customer"
