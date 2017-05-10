e55135de330593a5d2bb7884d98713b43490aae6
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-05-09 21:40:38 -0700

    fixed index method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
   def index
-    movies = Customer.all
+    customers = Customer.all
     render json: movies.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count])
   end
 
