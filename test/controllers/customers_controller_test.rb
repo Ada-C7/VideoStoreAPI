4402da5472337d23607a3d10dd815d43087d169f
Tamiko Terada <tamikodevo@gmail.com>
2017-05-10 15:16:25 -0700

    TDD tests for JSON for customers#index

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -8,6 +8,32 @@ describe CustomersController do
       get customers_url
       must_respond_with :success
     end
+
+    it "returns json" do
+      get customers_url
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an array" do
+      get customers_url
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the customers" do
+      get customers_url
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+
+    it "returns customers with exactly the required fields" do
+      keys = %w(name resistered_at postal_code phone movies_checked_out)
+      get customers_url
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.sort.must_equal keys
+      end
+    end
   end
 end
 
