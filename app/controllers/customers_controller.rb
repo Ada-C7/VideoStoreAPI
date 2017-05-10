c99020eeb4ac3d07ba274f9f0ac431b79afd8a3b
natalia-ku <bilberryfm@mail.ru>
2017-05-10 09:23:47 -0700

    added serializer for customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,7 +5,7 @@ class CustomersController < ApplicationController
     if customers.empty?
       render status: :internal_server_error, json: { errors: "Something went horribly wrong with our database. Sorry for inconvenience" }
     else
-      render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]), status: :ok
+      render json: customers, status: :ok
     end
   end
 
@@ -14,7 +14,7 @@ class CustomersController < ApplicationController
     if customer == nil
       render status: :not_found, json: { errors: "Cannot find a customer with id #{params[:id]}" }
     else
-      render json: customer.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]), status: :ok
+      render json: customer, status: :ok
     end
   end
 
