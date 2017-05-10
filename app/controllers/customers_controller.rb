902b26c0b8b74cfa01a62f4b6d623505b4dc8661
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-05-10 10:45:20 -0700

    Updated output for customers#show

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
   def index
     customers = Customer.all
-    render json: customers.as_json(only: ["name", "phone", "postal_code", "registered_at"]), status: :ok
+    render json: customers.as_json(only: ["id", "movies_checked_out", "name", "phone", "postal_code", "registered_at"]), status: :ok
   end
 end
