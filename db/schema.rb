13419ab121b19cc9a44be301e0c44de4e4699ad6
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-05-10 23:50:20 -0700

    Added availble_inventory column to movie table and updated all to be equal to inventory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511060238) do
+ActiveRecord::Schema.define(version: 20170511062936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,9 @@ ActiveRecord::Schema.define(version: 20170511060238) do
     t.string   "overview"
     t.string   "release_date"
     t.integer  "inventory"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.integer  "available_inventory"
   end
 
   create_table "rentals", force: :cascade do |t|
