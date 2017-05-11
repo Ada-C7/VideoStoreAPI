1faa60e35d8ab6a70e410ecee625ceedc6eb23d0
laura <lmelgarejos@unal.edu.co>
2017-05-11 10:12:30 -0700

    Smoke tests passing

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,7 +4,7 @@ class CustomersController < ApplicationController
     customers = Customer.all
 
     unless customers.count == 0
-      render :json => customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checkout_out_count]), status: :ok
+      render :json => customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]), status: :ok
     else
       render :json, status: :no_content
     end
