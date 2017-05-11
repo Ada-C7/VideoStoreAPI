779f024352aaf464a00190b8c62a8e14b791c2f3
Regan Huff <regan.huff@gmail.com>
2017-05-11 13:32:43 -0700

    WIP sort tests

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -28,25 +28,27 @@ CUSTOMER_KEYS = %w(id movies_checked_out_count name phone postal_code registered
       body.each do |customer|
         customer.keys.sort.must_equal CUSTOMER_KEYS
       end
-
     end
-
   end
 
   describe "index with sort parameter" do
     it "sorts customers by name" do
         get customers_path(sort: name)
-    end
-
-    it "sorts customers by date registered" do
-
-    end
-
-    it "sorts customers by postal code" do
+        # check first and last against sorted db
+        body = JSON.parse(response.body)
+        body.each do |customer|
 
     end
 
-    it "reverts to rails default if sort value is not valid" do
+    it "sorts customers by date registered" do skip
+
+    end
+
+    it "sorts customers by postal code" do skip
+
+    end
+
+    it "reverts to rails default if sort value is not valid" do skip
 
     end
   end
