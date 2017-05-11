2d432a1cc5752a3550c773415a64dae39b6449c1
natalia-ku <bilberryfm@mail.ru>
2017-05-10 19:54:25 -0700

    added update method for rentals

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -6,7 +6,6 @@ class RentalsController < ApplicationController
   def create
     # example of params
     # {"rental"=>{ "customer_id"=>1 }, "title"=>"Psycho"}
-
     movie = Movie.find_by(title: params[:title])
     if movie
       rental_info = {
@@ -26,15 +25,15 @@ class RentalsController < ApplicationController
   end
 
   def update
-    rental = Rental.find_by(title: params[:title], customer_id: params[:customer_id])
+    movie = Movie.find_by(title: params[:title])
+    rental = Rental.find_by(movie_id: movie.id, customer_id: params["rental"]["customer_id"])
     rental.return_date = Date.today
-
-    rental.customer.movies_checked_out_count += 1
-    rental.movie.available_inventory += 1 # ??????????
     rental.status = "checked in"
 
+    # rental.customer.movies_checked_out_count += 1
+    # rental.movie.available_inventory += 1 # ??????????
     if rental.save
-      render status: :ok, json: { id: rental.status }
+      render status: :ok, json: { status: rental.status }
     else
       render status: :bad_request, json: { errors: rental.errors.messages }
     end
@@ -43,9 +42,7 @@ class RentalsController < ApplicationController
 
   def overdue_rentals
     overdue_rentals = Rental.overdue_movies
-
     render json: overdue_rentals, each_serializer: OverdueListSerializer, status: :ok
-
   end
 
   private
