95898533fa878a25ef7080251d45e310bfcecd5b
Regan Huff <regan.huff@gmail.com>
2017-05-10 20:14:57 -0700

    Add tests and logic for invalid customer id or movie title

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,27 +1,37 @@
 class RentalsController < ApplicationController
 
   def update
-    # movie_id = find_movie_id(params[:title])
-    # find the rental in question based on params[:title] and params[:customer_id]
-    customer_id = params[:customer_id]
-    movie_id = Movie.find_by(title: params[:title])
-
-    rental = Rental.find_by(customer_id: customer_id, movie_id: movie_id)
-    # if found, set checkin_date to today's date
-    if rental
-      rental.checkin_date = Date.today
-      rental.save
-      render status: :ok, json: { nothing: true }
+    movie = find_movie(params[:title])
+    customer = find_customer(params[:customer_id])
+    if movie.nil?
+      # if movie title not found
+      render status: :bad_request, json: { error: "no movie found with that title"}
+    elsif customer.nil?
+      # if customer id not valid
+      render status: :bad_request, json: { error: "no customer found"}
+    else
+      # find the rental in question based on params[:title] and params[:customer_id]
+      rental = Rental.find_by(customer_id: customer.id, movie_id: movie.id)
+      # if found, set checkin_date to today's date
+      if rental
+        rental.checkin_date = Date.today
+        rental.save
+        render status: :ok, json: { nothing: true }
+      else
+        # if rental doesn't exist (customer id and movie id valid but movie never checked out)
+        render status: :bad_request, json: { error: "that movie is not checked out to that customer"}
+      end
     end
 
-
-    # if not found, do various things based on
-    # if customer id not valid
-    # if movie title not found
-    # if rental doesn't exist (customer id and movie id valid but movie never checked out)
   end
 
-  # def find_movie_id(title)
-  #   Movie.find_by_title(title).id
-  # end
+  private
+
+  def find_movie(title)
+    Movie.find_by_title(title)
+  end
+
+  def find_customer(id)
+    Customer.find_by_id(id)
+  end
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -10,11 +10,7 @@ describe RentalsController do
     let(:rental) { rentals(:one) }
     let(:customer) { customers(:good_data) }
     let(:movie) { movies(:psycho) }
-
-    # before do
-    #
-    #   # post checkin_path(Movie.first.title), params: { customer_id: Customer.first.id }
-    # end
+    let(:customer2) { customers(:no_rentals)}
 
     it "succeeds for valid title and customer id" do
       #valid means: rental exists [and therefore movie and customer exist]
@@ -27,12 +23,26 @@ describe RentalsController do
       rental.checkin_date.must_equal Date.today
     end
 
-    it "renders bad request for invalid customer id data" do skip
-      #customer id not found
+    it "modifies the db" do
+      proc {
+        post checkin_path(movie.title), params: { customer_id: customer.id }
+      }.must_change rental.checkin_date
     end
 
-    it "renders a different message if rental does not exist" do skip
+    it "renders bad request for invalid customer id data" do
+      #customer id not found
+      post checkin_path(movie.title), params: { customer_id: (Customer.last.id + 1)}
+      must_respond_with :bad_request
+      body = JSON.parse(response.body)
+      body["error"].must_include "customer"
+    end
+
+    it "renders a different message if rental does not exist" do
       #rental does not exist (customer does not actually have that movie checked out)
+      post checkin_path(movie.title), params: { customer_id: customer2.id}
+      must_respond_with :bad_request
+      body = JSON.parse(response.body)
+      body["error"].must_include "not checked out"
     end
 
     it "returns status not found if movie not found" do skip
