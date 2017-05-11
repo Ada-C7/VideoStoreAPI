a8fc80e940f5add9ef27bca278f5d4a79c16b834
Leesyruiz <leesyruiz@gmail.com>
2017-05-11 09:11:32 -0700

    working on tests

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,15 @@
 class Customer < ApplicationRecord
   has_many :movies, through: :rentals
 
-
+  validates :name, presence: true
+  validates :registered_at, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :postal_code, presence: true
+  validates :phone, presence: true
+  validates :account_credit, presence: true
+  validates :account_credit, presence: true
 
 
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,16 +1,17 @@
-# As with all Rails projects, model testing is a requirement. You should have at least one positive and one negative test case for each relation, validation, and custom function you add to your models.
-#
-# Use good TDD practices, and test before you code. Remember: red-green-refactor.
-
 require "test_helper"
 
 describe Customer do
   let(:customer) { Customer.new }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  it "all customers must be valid" do
+    :one.must_be :valid?
   end
+end
 
-
-
+# describe "validation tests" do
+#   it "Customer must be present" do
+#     customer[:one] = nil
+#     customer.save
+#     customer.must_be :invalid?
+#   end
 end
