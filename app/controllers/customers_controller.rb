29b40b4c6dbbc35d2e87ffd2dbcb81c8accd6b30
AlisonZ <alisonkzerbe@gmail.com>
2017-05-11 10:07:42 -0700

    customer controller tests done

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
     def index
         customers = Customer.all
-        render json: customers.as_json(except: [:created_at, :updated_at], methods: :movies_checked_out_count), status: :ok
+        render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone], methods: :movies_checked_out_count), status: :ok
     end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -20,5 +20,33 @@ describe CustomersController do
             get customers_path
             must_respond_with :success
         end
+
+        it "returns json" do
+            get customers_url
+            response.header['Content-Type'].must_include 'json'
+        end
+
+        it "returns an Array" do
+            get customers_url
+
+            body = JSON.parse(response.body)
+            body.must_be_kind_of Array
+        end
+
+        it "returns all of the customers" do
+            get customers_url
+
+            body = JSON.parse(response.body)
+            body.length.must_equal Customer.count
+
+        end
+        it "returns customers with exactly the required fields" do
+            keys = %w(id movies_checked_out_count name phone postal_code registered_at )
+            get customers_url
+            body = JSON.parse(response.body)
+            body.each do |customer|
+                customer.keys.sort.must_equal keys
+            end
+        end
     end
 end
