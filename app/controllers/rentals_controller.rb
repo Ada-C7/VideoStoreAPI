670d7195c6baa1e15406402386a6d2196362630e
Ann Dai <gofarann@gmail.com>
2017-05-11 10:15:02 -0700

    added decrease inventory logic

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,9 +3,6 @@ class RentalsController < ApplicationController
 
   def create
     movie = Movie.find_by_title(params[:title])
-    # if movie.inventory < 1
-    #   render status: :error, json: { error: "not enough inventory"}
-    # end
 
     if movie.nil? || movie.inventory < 1
       id = 0
@@ -16,13 +13,13 @@ class RentalsController < ApplicationController
     @rental = Rental.new(movie_id: id, customer_id: rental_params[:customer_id], due_date: rental_params[:due_date] )
 
     if @rental.save
+      movie.inventory -= 1
+      movie.save
       render status: :ok, json: @rental.as_json
     else
-      # @rental.errors.full_messages.each do |message|
-      #   puts message
-      # end
       render status: :error, json: { error: @rental.errors.full_messages }
     end
+  end
 
 
   def update
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -5,128 +5,141 @@ describe RentalsController do
   describe "Create (checkout)" do
 
     it "given valid customer id and movie title, can checkout movie (increase rental record by 1)" do
-    proc {
-       post checkout_path("Psycho"), params: {
-         customer_id: Customer.all.first.id, #this is going to AR to get the actual record but is talkign to the fixtures
-         due_date: Chronic.parse("two weeks from today")
+      proc {
+        post checkout_path("Psycho"), params: {
+          customer_id: Customer.all.first.id, #this is going to AR to get the actual record but is talkign to the fixtures
+          due_date: Chronic.parse("two weeks from today")
 
-       }
-     }.must_change 'Rental.count', 1
-     must_respond_with :success
+        }
+      }.must_change 'Rental.count', 1
+      must_respond_with :success
     end
 
     it "given invalid customer_id, cannot checkout movie" do
       proc {
-         post checkout_path("Psycho"), params: {
-           customer_id: Customer.all.last.id+1,
-           due_date: Chronic.parse("two weeks from today")
-         }
-       }.must_change 'Rental.count', 0
-       must_respond_with :error
+        post checkout_path("Psycho"), params: {
+          customer_id: Customer.all.last.id+1,
+          due_date: Chronic.parse("two weeks from today")
+        }
+      }.must_change 'Rental.count', 0
+      must_respond_with :error
 
-       body = JSON.parse(response.body)
-        body.must_be_kind_of Hash
-        body.must_include "error"
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include "error"
     end
 
     it "given invalid movie_title, cannot checkout movie" do
       proc {
-         post checkout_path("Psychosss"), params: {
-           customer_id: Customer.all.first.id,
-           due_date: Chronic.parse("two weeks from today")
-         }
-       }.must_change 'Rental.count', 0
-       must_respond_with 500
+        post checkout_path("Psychosss"), params: {
+          customer_id: Customer.all.first.id,
+          due_date: Chronic.parse("two weeks from today")
+        }
+      }.must_change 'Rental.count', 0
+      must_respond_with 500
 
-       body = JSON.parse(response.body)
-        body.must_be_kind_of Hash
-        body.must_include "error"
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include "error"
     end
 
     it "can checkout a movie with enough inventory" do
       proc {
-         post checkout_path("Psycho"), params: {
-           customer_id: Customer.all.first.id,
-           due_date: Chronic.parse("two weeks from today")
-         }
-       }.must_change 'Rental.count', 1
-       must_respond_with :success
+        post checkout_path("Psycho"), params: {
+          customer_id: Customer.all.first.id,
+          due_date: Chronic.parse("two weeks from today")
+        }
+      }.must_change 'Rental.count', 1
+      must_respond_with :success
     end
 
     it "cannot checkout movie with no inventory" do
       proc {
-         post checkout_path("Bring it On"), params: {
-           customer_id: Customer.all.first.id,
-           due_date: Chronic.parse("two weeks from today")
-         }
-        }.must_change 'Rental.count', 0
-       must_respond_with 500
+        post checkout_path("Bring it On"), params: {
+          customer_id: Customer.all.first.id,
+          due_date: Chronic.parse("two weeks from today")
+        }
+      }.must_change 'Rental.count', 0
+      must_respond_with 500
 
-       body = JSON.parse(response.body)
-        body.must_be_kind_of Hash
-        body.must_include "error"
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include "error"
 
     end
 
     it "returns error when due date is in the past" do
       proc {
-         post checkout_path("Psycho"), params: {
-           customer_id: Customer.all.first.id,
-           due_date: Chronic.parse("yesterday")
-         }
-       }.must_change 'Rental.count', 0
-       must_respond_with 500
+        post checkout_path("Psycho"), params: {
+          customer_id: Customer.all.first.id,
+          due_date: Chronic.parse("yesterday")
+        }
+      }.must_change 'Rental.count', 0
+      must_respond_with 500
 
-       body = JSON.parse(response.body)
-        body.must_be_kind_of Hash
-        body.must_include "error"
-
-
-  describe "check_in" do
-    let(:rental) { rentals(:one) }
-    let(:customer) { customers(:good_data) }
-    let(:movie) { movies(:psycho) }
-    let(:customer2) { customers(:no_rentals)}
-
-    it "succeeds for valid title and customer id" do
-      #valid means: rental exists [and therefore movie and customer exist]
-      post checkin_path(movie.title), params: { customer_id: customer.id }
-      must_respond_with :success
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include "error"
     end
 
-    it "modifies the db with today's date as checkin date" do
-      post checkin_path(movie.title), params: { customer_id: customer.id }
-      rental.checkin_date.must_equal Date.today
-    end
-
-    it "modifies the db" do
+    it "decreases inventory of movie by 1" do
       proc {
-        post checkin_path(movie.title), params: { customer_id: customer.id }
-      }.must_change rental.checkin_date
-    end
+        post checkout_path("Psycho"), params: {
+          customer_id: Customer.all.first.id,
+          due_date: Chronic.parse("two weeks from today")
+          }
+        }.must_change 'Movie.find_by_title("Psycho").inventory', -1
 
-    it "renders bad request for invalid customer id data" do
-      post checkin_path(movie.title), params: { customer_id: (Customer.last.id + 1)}
-      must_respond_with :bad_request
-      body = JSON.parse(response.body)
-      body["error"].must_include "customer"
     end
-
-    it "renders bad request if movie not found" do
-      post checkin_path("totally made up title"), params: { customer_id: customer.id }
-      must_respond_with :bad_request
-      body = JSON.parse(response.body)
-      body["error"].must_include "movie"
-    end
-
-    it "renders a different message if rental does not exist" do
-      #rental does not exist (customer does not actually have that movie checked out)
-      post checkin_path(movie.title), params: { customer_id: customer2.id}
-      must_respond_with :bad_request
-      body = JSON.parse(response.body)
-      body["error"].must_include "not checked out"
-    end
-
 
   end
-end
+
+
+    describe "check_in" do
+      let(:rental) { rentals(:one) }
+      let(:customer) { customers(:good_data) }
+      let(:movie) { movies(:psycho) }
+      let(:customer2) { customers(:no_rentals)}
+
+      it "succeeds for valid title and customer id" do
+        #valid means: rental exists [and therefore movie and customer exist]
+        post checkin_path(movie.title), params: { customer_id: customer.id }
+        must_respond_with :success
+      end
+
+      it "modifies the db with today's date as checkin date" do
+        post checkin_path(movie.title), params: { customer_id: customer.id }
+        rental.checkin_date.must_equal Date.today
+      end
+
+      it "modifies the db" do
+        proc {
+          post checkin_path(movie.title), params: { customer_id: customer.id }
+        }.must_change rental.checkin_date
+      end
+
+      it "renders bad request for invalid customer id data" do
+        post checkin_path(movie.title), params: { customer_id: (Customer.last.id + 1)}
+        must_respond_with :bad_request
+        body = JSON.parse(response.body)
+        body["error"].must_include "customer"
+      end
+
+      it "renders bad request if movie not found" do
+        post checkin_path("totally made up title"), params: { customer_id: customer.id }
+        must_respond_with :bad_request
+        body = JSON.parse(response.body)
+        body["error"].must_include "movie"
+      end
+
+      it "renders a different message if rental does not exist" do
+        #rental does not exist (customer does not actually have that movie checked out)
+        post checkin_path(movie.title), params: { customer_id: customer2.id}
+        must_respond_with :bad_request
+        body = JSON.parse(response.body)
+        body["error"].must_include "not checked out"
+      end
+
+
+    end
+  end
