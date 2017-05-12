ca8a4fa9b4f7c15e55b390da271b19960348ceab
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-05-12 10:33:20 -0700

    movie tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 describe Customer do
   let(:customer) { Customer.new }
-  #
+  # 
   # it "must be valid" do
   #   value(customer).must_be :valid?
   # end
@@ -11,7 +11,6 @@ describe Customer do
       customer.account_credit = 12.00
       customer.address = "123 Bob Lives Here"
       customer.city = "Seattle"
-      # id
       customer.name = "Bob Smart"
       customer.phone = "(123) 456-7890"
       customer.postal_code = "12345"
@@ -22,7 +21,16 @@ describe Customer do
     end
 
     it "is invalid without a name" do
-
+      customer.account_credit = 2.00
+      customer.address = "456 Grr Lane"
+      customer.city = "Seattle"
+      # customer.name =
+      customer.phone = "(123) 456-7890"
+      customer.postal_code = "12345"
+      customer.registered_at = "Fri, 15 Apr 2016 07:54:14 -0700"
+      customer.state = "KS"
+      # customer.save! can't have this idk why
+      customer.valid?.must_equal false
     end
   end
 end
