b625ff9199d4d7dd171297d1bb877e0975c82295
Regan Huff <regan.huff@gmail.com>
2017-05-12 14:21:10 -0700

    Tests and action to accept sort parameter for customer index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,12 @@
 class CustomersController < ApplicationController
 
   def index
-    customers = Customer.all
+    if ["name", "registered_at", "postal_code"].include?(params[:sort])
+      customers = Customer.order(params[:sort])
+    else
+      customers = Customer.all
+    end
+
     render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone],
     methods: :movies_checked_out_count), status: :ok
   end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -32,23 +32,32 @@ CUSTOMER_KEYS = %w(id movies_checked_out_count name phone postal_code registered
 
   describe "index with sort parameter" do
     it "sorts customers by name" do
-        get customers_path(sort: name)
+        get customers_path(sort: "name")
         # check first and last against sorted db
         body = JSON.parse(response.body)
-        body.each do |customer|
-
+        customers = Customer.order(:name)
+        body.first["name"].must_equal customers.first[:name]
+        body.last["name"].must_equal customers.last[:name]
     end
 
-    it "sorts customers by date registered" do skip
-
+    it "sorts customers by date registered" do
+      get customers_path(sort: "registered_at")
+      # check first and last against sorted db
+      body = JSON.parse(response.body)
+      customers = Customer.order(:registered_at)
+      body.first["registered_at"].must_equal customers.first[:registered_at]
+      body.last["registered_at"].must_equal customers.last[:registered_at]
     end
 
-    it "sorts customers by postal code" do skip
-
+    it "sorts customers by postal code" do
+      get customers_path(sort: "postal_code")
+      # check first and last against sorted db
+      body = JSON.parse(response.body)
+      customers = Customer.order(:postal_code)
+      body.first["postal_code"].must_equal customers.first[:postal_code]
+      body.last["postal_code"].must_equal customers.last[:postal_code]
     end
 
-    it "reverts to rails default if sort value is not valid" do skip
 
-    end
   end
 end
