f23035762fb889bbedec8ebcb8fcf3172e36e1db
Tamiko Terada <tamikodevo@gmail.com>
2017-05-12 15:05:00 -0700

    add rentals columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512172013) do
+ActiveRecord::Schema.define(version: 20170512215235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,11 @@ ActiveRecord::Schema.define(version: 20170512172013) do
   end
 
   create_table "rentals", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "customer_id"
+    t.integer  "movie_id"
+    t.string   "due_date"
   end
 
 end
