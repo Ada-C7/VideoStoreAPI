1a0de30f23bc96fde831429d1d5492c014ee174d
Andrea Valliere <amvalliere@gmail.com>
2017-05-12 12:11:06 -0700

    Added serializer for customer and movie

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,8 +1,9 @@
 class CustomersController < ApplicationController
   def index
     customers = Customer.all
-    render json: customers.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :account_credit]), status: :ok
+    render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]), status: :ok
   end
+
 
   # def show
   #   customer = Customer.find_by(id: params[:id])
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,9 @@ class Customer < ApplicationRecord
   has_many :rentals
   has_many :movies, :through => :rentals
   validates :name, presence: true
+
+  def movies_checked_out_count
+    @count = 0
+    # render :json => @count.to_json(:methods => :movies_checked_out_count)
+  end
 end
