b23663070d98a5abb4717ba7fd671d5dad1e92d9
Brenna <bcmdarroch@gmail.com>
2017-05-12 11:22:07 -0700

    got checkout and checkin working, tested in postman

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,42 +1,43 @@
 class RentalsController < ApplicationController
 
-    def checkout
-        #add method to check if there is enough in available_inventory
-        rental = Rental.new
-        rental.movie = Movie.find_by_title(params[:title])
-        rental.customer = Customer.find_by_id(params[:rental][:customer_id])
-        rental.checkout_date = Date.today
-        rental.due_date = Date.today + 7
+  def checkout
+    #add method to check if there is enough in available_inventory
+    rental = Rental.new
+    rental.movie = Movie.find_by_title(params[:title])
+    rental.customer = Customer.find_by_id(params[:rental][:customer_id])
+    rental.checkout_date = Date.today
+    rental.due_date = Date.today + 7
+    rental.checked_out = true
+    rental.movie.available_inventory -= 1
 
-        if rental.save
-            render json: { id: rental.id }, status: :ok
-            rental.movie.available_inventory -=1
-            rental.checked_out = true
-            rental.movie.save
-        else
-            render json: { errors: rental.errors.messages }, status: :bad_request
-        end
-
+    if rental.save && rental.movie.save
+      render json: { id: rental.id, checked_out: rental.checked_out }, status: :ok
+    else
+      render json: { errors: rental.errors.messages }, status: :bad_request
     end
 
-    def checkin
-        movie = Movie.find_by_title(params[:title])
-        customer = Customer.find_by_id(params[:customer][:id])
-        rental = Rental.find_by_movie_id_and_customer_id(movie.id, customer.id)
-        rental.checked_out = false
-        rental.available_inventory +=1
-        if rental.save
-            render json: { id: rental.id }, status: :ok
-        else
-            render json: { errors: rental.errors.messages }, status: :bad_request    
-        end
-    end
+  end
 
-    def overdue
-        # return list of rentals
-        rentals = Rental.overdue?
-        render json: rentals.as_json()
-        # return title, customer_id, name, postal_code, checkout_date, due_date
+  def checkin
+    movie_id = Movie.find_by_title(params[:title]).id
+    customer_id = Customer.find_by_id(params[:rental][:customer_id])
+    rental = Rental.find_by_movie_id_and_customer_id(movie_id, customer_id)
+
+    rental.checked_out = false
+    rental.movie.available_inventory += 1
+
+    if rental.save && rental.movie.save
+      render json: { id: rental.id, checked_out: rental.checked_out }, status: :ok
+    else
+      render json: { errors: rental.errors.messages }, status: :bad_request
     end
+  end
+
+  def overdue
+    # return list of rentals
+    rentals = Rental.overdue?
+    render json: rentals.as_json()
+    # return title, customer_id, name, postal_code, checkout_date, due_date
+  end
 
 end
