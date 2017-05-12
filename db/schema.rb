ff54e6fc711f582d1c6cbae3dffc6f1c67982afa
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-05-11 22:04:25 -0700

    ChangeDueDateType to datetime

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511062936) do
+ActiveRecord::Schema.define(version: 20170512045503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,13 +39,13 @@ ActiveRecord::Schema.define(version: 20170511062936) do
   end
 
   create_table "rentals", force: :cascade do |t|
-    t.string   "due_date"
     t.datetime "checkin_date"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.integer  "customer_id"
     t.integer  "movie_id"
     t.boolean  "returned",     default: false
+    t.datetime "due_date"
     t.index ["customer_id"], name: "index_rentals_on_customer_id", using: :btree
     t.index ["movie_id"], name: "index_rentals_on_movie_id", using: :btree
   end
