d38104cdee693fcd19eec84bd2bf462da7963cee
Brenna <bcmdarroch@gmail.com>
2017-05-12 13:04:35 -0700

    rental controller test stubs

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  describe "checkout" do
+
+  end
+
+  describe "checkin" do
+    # changes rental status
+
+    # 
+  end
+
+  describe "overdue" do
+    # gets overdue rentals
+
+    # works if no overdue rentals
+
+    # all rentals in list are past due
+  end
 end
