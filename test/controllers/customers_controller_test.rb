a962fb669f89b781e494de969cc16b259ed69731
amb54 <ambarklund@hotmail.com>
2017-05-12 15:44:59 -0700

    added tests to the cutomer model and controller and updated customer model

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -17,8 +17,19 @@ describe CustomersController do
     it "returns an array" do
       get customers_url
       # response.body.must_be_kind_of Array  # response.body is a string
-      body = JSON.parse(response.body)
+      body = JSON.parse(response.body) # puts body gives the data in yml
       body.must_be_kind_of Array
+    end
+
+    it "returns an empty array if the model is empty" do
+      Rental.destroy_all
+      Customer.destroy_all
+      get customers_url
+      # response.body.must_be_kind_of Array  # response.body is a string
+      body = JSON.parse(response.body) # puts body gives the data in yml
+      body.must_be_kind_of Array
+      body.must_be_empty
+      body.must_equal []
     end
 
     it "returns all customers" do
@@ -26,9 +37,17 @@ describe CustomersController do
       body = JSON.parse(response.body)
       body.length.must_equal Customer.count
     end
+
+
+
   end # END of describe "index"
 
-  describe "output from customer_serializer" do
+  describe "output for a customer" do
+    it "returns value for id" do
+      get customers_url
+      body = JSON.parse(response.body)
+      body[0].must_include "id"
+    end
 
     it "returns value for name" do
       get customers_url
@@ -36,7 +55,25 @@ describe CustomersController do
       body[0].must_include "name"
     end
 
-    it "returns value for movies_checked_out_count" do
+    it "returns value for registered_at" do
+      get customers_url
+      body = JSON.parse(response.body)
+      body[0].must_include "registered_at"
+    end
+
+    it "returns value for postal_code" do
+      get customers_url
+      body = JSON.parse(response.body)
+      body[0].must_include "postal_code"
+    end
+
+    it "returns value for phone" do
+      get customers_url
+      body = JSON.parse(response.body)
+      body[0].must_include "phone"
+    end
+
+    it "returns value for movies_checked_out_count (from customer_serializer)" do
       get customers_url
       body = JSON.parse(response.body)
       body[0].must_include "movies_checked_out_count"
@@ -48,7 +85,7 @@ describe CustomersController do
       body[0].wont_include "city"
     end
 
-  end # END of describe "output from customer_serializer" 
+  end # END of describe "output from customer_serializer"
 
 
 
