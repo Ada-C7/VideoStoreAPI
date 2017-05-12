d7394fa6c07b4bdad2d874113466821df6bf4f79
Bo Trethewey <botrethewey@gmail.com>
2017-05-12 10:27:50 -0700

    updated tests and controller logic

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -4,7 +4,6 @@ describe RentalsController do
   describe "create" do
     let(:rental_data) {
       {
-        title: movies(:one).title,
         customer_id: customers(:one).id,
         due_date: "2017-06-30"
       }
@@ -12,8 +11,7 @@ describe RentalsController do
 
     it "Creates a new rental" do
       proc {
-        post check_out_path(rental_data[:title]), params: { rental: rental_data }
-
+        post check_out_path(movies(:one).title), params: { rental: rental_data }
       }.must_change 'Rental.count', 1
       must_respond_with :success
       body = JSON.parse(response.body)
@@ -21,18 +19,15 @@ describe RentalsController do
       body.must_include "id"
 
       # Check that the ID matches
-      Rental.find(body["id"]).movie.title.must_equal rental_data[:title]
+      Rental.find(body["id"]).movie.title.must_equal movies(:one).title
       Rental.find(body["id"]).customer_id.must_equal rental_data[:customer_id]
       Rental.find(body["id"]).due_date.must_equal Date.parse(rental_data[:due_date])
     end
 
     # may need more tests for cases: missing customer, missing duedate
     it "Returns an error for an invalid rental" do
-      bad_data = rental_data.clone()
-      bad_data.delete(:title)
-
       proc {
-        post check_out_path(rental_data[:title]), params: { rental: bad_data }
+        post check_out_path("jaws"), params: { rental: rental_data }
       }.wont_change 'Rental.count'
       must_respond_with :bad_request
 
