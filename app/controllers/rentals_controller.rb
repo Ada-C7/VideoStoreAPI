43ec255d84845c4692fa68951bb6a0f65a8ae795
AlisonZ <alisonkzerbe@gmail.com>
2017-05-12 10:31:11 -0700

    checkin method

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,31 +1,42 @@
 class RentalsController < ApplicationController
 
     def checkout
-      rental = Rental.new
-      rental.movie = Movie.find_by_title(params[:title])
-      rental.customer = Customer.find_by_id(params[:rental][:customer_id])
-      rental.checkout_date = Date.today
-      rental.due_date = Date.today + 7
+        #add method to check if there is enough in available_inventory
+        rental = Rental.new
+        rental.movie = Movie.find_by_title(params[:title])
+        rental.customer = Customer.find_by_id(params[:rental][:customer_id])
+        rental.checkout_date = Date.today
+        rental.due_date = Date.today + 7
 
-      if rental.save
-        render json: { id: rental.id }, status: :ok
-      else
-        render json: { errors: rental.errors.messages }, status: :bad_request
-      end
+        if rental.save
+            render json: { id: rental.id }, status: :ok
+            rental.movie.available_inventory -=1
+            rental.checked_out = true
+            rental.movie.save
+        else
+            render json: { errors: rental.errors.messages }, status: :bad_request
+        end
 
     end
 
     def checkin
-      # parameters: title
-      # request customer_id
-
+        movie = Movie.find_by_title(params[:title])
+        customer = Customer.find_by_id(params[:customer][:id])
+        rental = Rental.find_by_movie_id_and_customer_id(movie.id, customer.id)
+        rental.checked_out = false
+        rental.available_inventory +=1
+        if rental.save
+            render json: { id: rental.id }, status: :ok
+        else
+            render json: { errors: rental.errors.messages }, status: :bad_request    
+        end
     end
 
     def overdue
-      # return list of rentals
-      rentals = Rental.overdue?
-      render json: rentals.as_json()
-      # return title, customer_id, name, postal_code, checkout_date, due_date
+        # return list of rentals
+        rentals = Rental.overdue?
+        render json: rentals.as_json()
+        # return title, customer_id, name, postal_code, checkout_date, due_date
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509213919) do
+ActiveRecord::Schema.define(version: 20170512172158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170509213919) do
     t.string   "due_date"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.boolean  "checked_out"
     t.index ["customer_id"], name: "index_rentals_on_customer_id", using: :btree
     t.index ["movie_id"], name: "index_rentals_on_movie_id", using: :btree
   end
