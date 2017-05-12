72230a9632914c4d17d76679f9a0086ae5db4909
Marisol Lopez <lmarisolopez@gmail.com>
2017-05-12 14:57:44 -0700

    changed checkout and checkin routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get '/rentals/overdue', to: 'customers#overdue', as: 'overdue'
 
-  post '/movies/:title/check-out', to: 'movies#checkout', as: 'checkout'
-  post '/movies/:title/check-in', to: 'movies#checkin', as: 'checkin'
+  post '/rentals/:title/check-out', to: 'movies#checkout', as: 'checkout'
+  post '/rentals/:title/check-in', to: 'movies#checkin', as: 'checkin'
 
 end
