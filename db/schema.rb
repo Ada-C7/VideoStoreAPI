7965b39948260d408e5690dbeb29e644ba63e058
Jackie Watanabe <sushiigirl@gmail.com>
2017-05-12 11:16:37 -0700

    added available_inventory column to movies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512181237) do
+ActiveRecord::Schema.define(version: 20170512181539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,12 +30,13 @@ ActiveRecord::Schema.define(version: 20170512181237) do
   end
 
   create_table "movies", force: :cascade do |t|
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "title"
     t.string   "overview"
     t.string   "release_date"
     t.integer  "inventory"
+    t.integer  "available_inventory"
   end
 
   create_table "rentals", force: :cascade do |t|
