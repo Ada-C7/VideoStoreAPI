3225eba279e3d10875085cbbb27d97336bad2682
Janice Lichtman <jalichtman@gmail.com>
2017-05-12 11:44:37 -0700

    added 2 rentals in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@ end
 JSON.parse(File.read('db/seeds/movies.json')).each do |movie|
   Movie.create!(movie)
 end
+
+
+Rental.create(customer_id: 1, movie_id: 1, due_date: Date.yesterday, checked_out: true)
+
+Rental.create(customer_id: 2, movie_id: 2, due_date: Date.yesterday, checked_out: true)
