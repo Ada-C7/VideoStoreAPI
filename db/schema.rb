6fc8e8ea2439cd5346702eb9a1ed7de2619af78c
Jackie Watanabe <sushiigirl@gmail.com>
2017-05-12 11:14:22 -0700

    added movies_checked_out_count column

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510001459) do
+ActiveRecord::Schema.define(version: 20170512181237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "customers", force: :cascade do |t|
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.string   "name"
     t.string   "registered_at"
     t.string   "address"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170510001459) do
     t.string   "postal_code"
     t.string   "phone"
     t.float    "account_credit"
+    t.integer  "movies_checked_out_count"
   end
 
   create_table "movies", force: :cascade do |t|
