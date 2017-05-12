b82c077342d33ac7c3aa05d0a2946cae8aa64603
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-12 16:01:22 -0700

    more spacing cleanuo

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,16 +1,16 @@
 class Customer < ApplicationRecord
   has_many :rentals
-  
+
   validates :name, presence: true
-  validates :registered_at,presence: true
+  validates :registered_at, presence: true
   validates :address, presence: true, length: {minimum: 3}
-  validates :city,presence: true, format: { with: /\A[a-zA-Z]+\z/ }
-  validates :state,presence: true, format: { with: /\A[a-zA-Z]+\z/}
-  validates :postal_code,presence: true, numericality: true, length: { is: 5 }
-  validates :phone,presence: true, format: { with: /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}\z/ }
+  validates :city, presence: true, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :state, presence: true, format: { with: /\A[a-zA-Z]+\z/}
+  validates :postal_code, presence: true, numericality: true, length: { is: 5 }
+  validates :phone, presence: true, format: { with: /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}\z/ }
 
   def increase_checkout_count
     self.movies_checked_out_count += 1
-    self.save if self.valid?
+    self.save
   end
 end
