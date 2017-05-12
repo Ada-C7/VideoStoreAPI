08aa6493b1d21455205a09d466f0bc273881d2f8
Kayla <kubicke.kayla@gmail.com>
2017-05-11 22:12:29 -0700

    All customers are the right type of object.

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -3,7 +3,15 @@ require "test_helper"
 describe Customer do
   let(:customer) { Customer.new }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  it "all customers in database are valid" do
+    customers = Customer.all
+    customers.each do |customer|
+      customer.must_be_kind_of Customer
+    end
   end
+
+  # No validations exist.
+  # it 'customer can not be created without required attributes' do
+  #   proc { Customer.new }.must_raise ArgumentError
+  # end
 end
