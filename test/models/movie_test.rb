9fd41cb65927c165f55b35f4f36d6cc5f80404ab
Anacelia Gillette <acgillette426@gmail.com>
2017-05-12 14:44:38 -0700

    Testing negative inventory passed

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -9,4 +9,9 @@ describe Movie do
     movies(:no_date).valid?.must_equal false
     movies(:no_inv).valid?.must_equal false
   end
+
+  it "must have an inventory that is not in the negative" do
+    movies(:starwars).valid?.must_equal true
+    movies(:neg_inv).valid?.must_equal false
+  end
 end
