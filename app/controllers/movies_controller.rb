7dfda2aa62811bf48819a611542ade11f6e6ad22
Anacelia Gillette <acgillette426@gmail.com>
2017-05-13 15:15:16 -0700

    Starting on movies show method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,13 @@ class MoviesController < ApplicationController
     render json: movies.as_json(only:
     [:title, :release_date ]), :status => :ok
   end
+
+  def show
+    movie = Movie.find_by title: params[:title]
+    if movie
+      render json: movie.as_json(only:
+      [:title, :release_date]), :status => :ok
+    end
+  end
+
 end
