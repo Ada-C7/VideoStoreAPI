8a34a8c3cacf9fab5e68427b046358ab29dee2de
Anacelia Gillette <acgillette426@gmail.com>
2017-05-13 14:54:36 -0700

    Basic validations for customers green

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,7 @@ class Customer < ApplicationRecord
   validates :registered_at, presence: true
   validates :address, presence: true
   validates :city, presence: true
+  validates :state, presence: true
   validates :postal_code, presence: true
   validates :phone, presence: true
   validates :account_credit, presence: true, numericality: true
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -4,5 +4,13 @@ describe Customer do
 
   it "must have all fields present" do
     customers(:sam).valid?.must_equal true
+    customers(:no_name).valid?.must_equal false
+    customers(:no_reg).valid?.must_equal false
+    customers(:no_add).valid?.must_equal false
+    customers(:no_city).valid?.must_equal false
+    customers(:no_state).valid?.must_equal false
+    customers(:no_code).valid?.must_equal false
+    customers(:no_phone).valid?.must_equal false
+    customers(:no_credit).valid?.must_equal false
   end
 end
