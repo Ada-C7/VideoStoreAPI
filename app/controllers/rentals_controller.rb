58f0afcf790593ed3798fae0afbf14f0a44d812e
Anacelia Gillette <acgillette426@gmail.com>
2017-05-13 22:19:59 -0700

    Working on rental check out tests

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,19 +1,22 @@
 class RentalsController < ApplicationController
 
   def create
-    rental = Rental.new
+    rental = Rental.new(rental_params)
     rental.movie = Movie.find_by title: params[:title]
-    rental.customer = Customer.find(params[:rental][:customer_id])
-    rental.due_date = params[:rental][:due_date]
     rental.check_out = Date.today
-     if rental.movie.available_inventory < 0
+     if rental.movie.available_inventory == 0
        render status: :bad_request, json: { error: "Not enough inventory" }
      elsif rental.save
        render status: :ok, json: { id: rental.id }
        rental.customer.movies_checked_out_count += 1
      else
-       render statis: :bad_request, json: { errors: rental.error.messages }
+       render statis: :bad_request, json: { errors: rental.errors.messages }
      end
   end
 
+  private
+
+  def rental_params
+    params.require(:rental).permit(:customer_id, :due_date)
+  end
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -11,7 +11,25 @@ describe RentalsController do
   it "Can create a new rental" do
     proc {
     post new_rental_path(movies(:starwars).title), params: { rental: info }
-  }.must_change 'Rental.count', 1
-  must_respond_with :success
+    }.must_change 'Rental.count', 1
+    must_respond_with :success
+  end
+
+  it "responds with an error if inventory goes over" do
+    post new_rental_path(movies(:jaws).title), params: { rental: info }
+    must_respond_with :success
+    post new_rental_path(movies(:jaws).title), params: { rental: info }
+    must_respond_with :bad_request
+    r = JSON.parse(response.body)
+    r.must_include "error"
+  end
+
+  it "does not create a rental with a bad request" do
+    bad_info = {
+        "due_date" => "2018-01-23"
+    }
+    
+    post new_rental_path(movies(:jaws).title), params: { rental: bad_info }
+    must_respond_with :bad_request
   end
 end
