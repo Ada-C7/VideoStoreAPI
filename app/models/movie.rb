e0df2769962530b3f9ee0a97870dda41ce5131b7
Kayla <kubicke.kayla@gmail.com>
2017-05-13 18:00:35 -0700

    All movie tests are passing.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,16 +1,19 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :release_date, presence: true
+
   has_many :customers, through: :customer_movie
   has_many :customer_movies
 
   def check_out(title_params)
-    movie = Movie.find_by(title: params[:title])
-    movie.available_inventory
+    # movie = Movie.find_by(title: params[:title])
+    # movie.available_inventory
   end
 
   def available_inventory
-    rentals = CustomerMovie.all
-    quantity = rentals.where(id: params[:movie_id]).count
-    a_i = inventory - quantity
+    # rentals = CustomerMovie.all
+    # quantity = rentals.where(id: params[:movie_id]).count
+    # a_i = inventory - quantity
   end
 
   def checkedoutmovies
