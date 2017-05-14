7007f947959d391a61b026b7cc43a6f0a529ba23
Anacelia Gillette <acgillette426@gmail.com>
2017-05-13 21:45:06 -0700

    Customer index tests complete

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -5,4 +5,15 @@ describe CustomersController do
     get customers_path
     must_respond_with :success
   end
+
+  it "the response must be of type json" do
+    get customers_path
+    response.header['Content-Type'].must_include "json"
+  end
+
+  it "returns the correct amount of customers" do
+    get customers_path
+    r = JSON.parse(response.body)
+    r.length.must_equal Customer.count
+  end
 end
