b37dd334cfd49a1ae76a4c0be55e97d654e7c10b
Anacelia Gillette <acgillette426@gmail.com>
2017-05-13 21:55:51 -0700

    Starting on rentals controller tests

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+    let(:info) {
+      {
+        "customer_id" => customers(:sam).id,
+        "due_date" => "2018-01-23"
+      }
+    }
+
+  it "Can create a new rental" do
+    proc {
+    post new_rental_path(movies(:starwars).title), params: { rental: info }
+  }.must_change 'Rental.count', 1
+  must_respond_with :success
+  end
 end
