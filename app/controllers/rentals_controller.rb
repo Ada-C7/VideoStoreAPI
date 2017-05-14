e7c7bee4c7bd4ce126bae072613a3cc268727ce1
Anacelia Gillette <acgillette426@gmail.com>
2017-05-13 18:08:09 -0700

    Got checkout functionality working

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -5,9 +5,12 @@ class RentalsController < ApplicationController
     rental.movie = Movie.find_by title: params[:title]
     rental.customer = Customer.find(params[:rental][:customer_id])
     rental.due_date = params[:rental][:due_date]
-     if rental.save
-       rental.movie.available_inventory = rental.movie.inventory - 1
+    rental.check_out = Date.today
+     if rental.movie.available_inventory < 0
+       render status: :bad_request, json: { error: "Not enough inventory" }
+     elsif rental.save
        render status: :ok, json: { id: rental.id }
+       rental.customer.movies_checked_out_count += 1
      else
        render statis: :bad_request, json: { errors: rental.error.messages }
      end
