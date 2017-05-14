88afb2e03815d0c6c5c061c500476d4e9156aac9
Kayla <kubicke.kayla@gmail.com>
2017-05-13 18:44:14 -0700

    All smoke tests pass.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  validates :id, presence: true
+  # validates :id, presence: true
   validates :name, presence: true
   validates :registered_at, presence: true
   validates :postal_code, presence: true
@@ -9,6 +9,10 @@ class Customer < ApplicationRecord
   has_many :movies, through: :customer_movies
   has_many :customer_movies
 
+  def movies_checked_out_count
+    # Default is zero right now.
+    return 0
+  end
 
   def check_out#(params)
     # customer = Customer.find_by(id: params[:customer_id])
