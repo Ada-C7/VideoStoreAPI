e21e58697dc5dec597c4a6f41226e0f706d833a0
Hyunji Kim <ricecakemonster@gmail.com>
2017-05-13 21:32:54 -0700

    paginate working

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -5,24 +5,26 @@ class RentalsController < ApplicationController
       render json: {errors: "No rentals found"}, status: :not_found
     else
       if params[:sort] == "title"
-        rentals = Rental.joins(:movie).merge(Movie.order(:title))
+        rentals = Rental.paginate(page: params[:p], per_page: params[:n]).joins(:movie).merge(Movie.order(:title))
       elsif params[:sort] == "name"
-        rentals = Rental.joins(:customer).merge(Customer.order(:name))
+        rentals = Rental.paginate(page: params[:p], per_page: params[:n]).joins(:customer).merge(Customer.order(:name))
       elsif params[:sort] == "checkout_date"
-        rentals = Rental.order(:created_at)
+        rentals = Rental.paginate(page: params[:p], per_page: params[:n]).order(:created_at)
       elsif params[:sort] == "due_date"
         Rental.all.each do |rental|
           rental.due_date.to_time
+          rental.save
         end
-        rentals = Rental.order(:due_date)
-        return rentals
+        rentals = Rental.paginate(page: params[:p], per_page: params[:n]).order(:due_date)
       end
     end
+
+    rentals = Rental.paginate(page: params[:p], per_page: params[:n]) if rentals.nil?
     current_rentals = rentals.where(returned_date: nil)
     if current_rentals.nil?
       render json: {errors: "No current rentals"}, status: :not_found
     end
-  
+
     rentals = current_rentals.where("due_date < ?", Time.now)
 
 
