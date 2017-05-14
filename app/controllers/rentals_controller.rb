f4ffbff756c7569f1f5ca94e368fdf8b79a3499e
Anacelia Gillette <acgillette426@gmail.com>
2017-05-13 17:17:13 -0700

    Started on check out functionality

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,12 @@
 class RentalsController < ApplicationController
+
+  def create
+    rental = Rental.new
+    rental.movie = Movie.find_by title: params[:title]
+    rental.customer = Customer.find(params[:rental][:customer_id])
+    rental.due_date = params[:rental][:due_date]
+    rental.save
+    render status: :ok, json: { id: rental.id }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get '/movies/:title', to: 'movies#show', as: 'movie'
 
   get '/customers', to: 'customers#index', as: 'customers'
+
+  post '/rentals/:title/checkout', to: 'rentals#create', as: 'new_rental'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
