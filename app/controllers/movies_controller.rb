76501ad81293cf926db44d5255aacbd7bf4f4ea5
janicewilson <janice.wilson@mac.com>
2017-05-15 11:16:32 -0700

    Not certain what this last update entails. But am pushing regardless.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,18 @@
 class MoviesController < ApplicationController
 
      def index
-       movies = Movie.all
-       render json: movies.as_json(only: [:title, :release_date]), status: :ok
+          movies = Movie.all
+          render json: movies.as_json(only: [:title, :release_date]), status: :ok
      end
 
      def show
-       movie = Movie.find_by(title: params[:title])
-       render json: movie.as_json(only: [:title, :overview, :release_date, :inventory, :available_inventory]), status: :ok
+          movie = Movie.find_by(title: params[:title])
+
+          if movie
+               render json: movie.as_json(only: [:title, :overview, :release_date, :inventory, :available_inventory]), status: :ok
+          else
+               render json: { errors:  { title: "#{params[:title]} was not found." } }, status: :not_found
+          end
      end
 
      private
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ApplicationRecord
+
+     validates :title, presence: { message: ': Can\'t find a movie without a    title.' },
+          length: { minimum: 2 }
+     # validates :status, inclusion: { in: [true, false], message: 'Are you coming? Or are abandoning us on our special day?' }
+     #
+     # validates :number_of_guests, presence: { message: 'It takes at least one to RSVP.' },
+     #      numericality: { only_integer: true, message:  'Is it just you? Or do you have a +1?' }
+     #
+     # validates :second_guest, presence: { message: 'Don\'t forget about your +1.' }, if: :plus_one?, on: :update,
+     #      uniqueness: { message: 'Oops. Looks like you\'ll need another +1. That particular +1 has already RSVP\'d.' }
+     #
+     # validates :paid, inclusion: { in: [true, false], message: 'Have you settled up?' }
 end
